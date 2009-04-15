08c13e60393e28b8030cdb58a6703ae5f1e3279b
Sven Bohm <bohms@msu.edu>
2009-04-14 22:02:35 -0400

    added meeting abstract new link and delete

--- app/controllers/meeting_abstracts_controller.rb
+++ app/controllers/meeting_abstracts_controller.rb
@@ -4,15 +4,37 @@ class MeetingAbstractsController < ApplicationController
   # GET meeting_abstracts
   # GET meeting_abstracts.xml
   def index
-    @abstracts = MeetingAbstracts.find(:all, :oder=> :authors)
+    @abstracts = MeetingAbstract.find(:all, :order=> :authors)
     respond_to do |format|
       format.html # index.erb
       format.xml { render :xml => @abstracts.to_xml}
     end
   end
+  
+  # GET meeting_abstracts/new?meeting=1
+  def new
+    @meeting_abstract = MeetingAbstract.new
+    meeting  = Meeting.find(params[:meeting])
+    @meeting_abstract.meeting_id = meeting.id
+  end
+  
+  
+  def create
+    @meeting_abstract = MeetingAbstract.new(params[:meeting_abstract])
+    respond_to do |format|
+      if @meeting_abstract.save
+        flash[:notice] = 'Meeting Abstract was successfully created.'
+        format.html { redirect_to meeting_url(@meeting_abstract.meeting) }
+        format.xml  { head :created, :location => meeting_abstracts_url(@meeting) }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @meeting_abstract.errors.to_xml }
+      end
+    end
+  end
 
   # GET meeting_abstracts/1
-  # gET meeting_abstracts/1.xml
+  # GET meeting_abstracts/1.xml
   def show
     @abstract = MeetingAbstract.find(params[:id])
 
@@ -46,10 +68,16 @@ class MeetingAbstractsController < ApplicationController
   
   def destroy
     @meeting_abstract = MeetingAbstract.find(params[:id])
+    meeting_abstract_id = "abstract_#{@meeting_abstract.id}"
     @meeting_abstract.destroy
      respond_to do |format|
        format.html { redirect_to meetings_url }
        format.xml  { head :ok }
+       format.js do 
+         render :update do |page|
+           page.visual_effect :fade, meeting_abstract_id
+         end
+       end
      end
     
   end
