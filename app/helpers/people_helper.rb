a0ad635597ad0ceef9b14d95afecddbe1db69bf2
Sven Bohm <bohms@msu.edu>
2009-09-16 13:21:33 -0600

    added parenthesis around titles

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -3,15 +3,10 @@ module PeopleHelper
   def contact_link(email)
     "<span id='email'>#{email.gsub(/@/,' at ')}</span>"
   end
-  
-  
-  def show_committee(committee_name, role)
-    html = committee_name
-    role.people.each do |person|
-     # next unless person.affiliations.find_by_role_id(role).title == committee_name
-      html = html  + "<% link_to #{person.full_name}, person_path(person)&nbsp;%>"
-    end
-    html
+    
+  def parenthize(string)
+    return string if string.nil?
+    return "("+string+")"
   end
   
   
