02bb118ea4bd3f8fbee6a7411d33c9cf895532ab
Sven Bohm <bohms@msu.edu>
2009-09-16 15:24:11 -0600

    commas between names on network

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,14 +1,28 @@
 module PeopleHelper
-  
+
   def contact_link(email)
     "<span id='email'>#{email.gsub(/@/,' at ')}</span>"
   end
-    
+
   def parenthize(string)
     return string if string.nil?
     return string if string.strip == ""
     return "("+string+")"
   end
-  
-  
+
+  def show_committee(committee,role)
+    html = "<li>#{committee}  "
+    role.people.each_with_index do |person,i|
+      next unless person.affiliations.find_by_role_id(role).title == committee
+      if i == 0
+        html += link_to "#{person.full_name}", person_path(person)
+      else
+        html += ", "
+        html += link_to "#{person.full_name}", person_path(person)
+      end 
+
+    end
+    html += "</li>"
+    html
+  end
 end
