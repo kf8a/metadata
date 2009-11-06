4b61ced1d55c2a1928637e4e945bbe2bc47189ed
Sven Bohm <bohms@msu.edu>
2009-11-05 21:49:07 -0500

    use the lter as asset host. this means that css has to be loaded separately

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
 
 # Enable serving of images, stylesheets, and javascripts from an asset server
-#config.action_controller.asset_host                  = "http://lter.kbs.msu.edu/dynamic"
+config.action_controller.asset_host                  = "http://lter.kbs.msu.edu/dynamic"
 
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
