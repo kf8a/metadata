635de54339713aa4360e03541623e69890364031
Sven Bohm <bohms@msu.edu>
2010-02-10 09:46:32 -0500

    removed variate fixture

--- test/unit/variate_test.rb
+++ test/unit/variate_test.rb
@@ -1,10 +1,5 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class VariateTest < ActiveSupport::TestCase
-  fixtures :variates
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
-  end
 end
