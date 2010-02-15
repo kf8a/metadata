7a229e0b2c14481368adf1735e70017a5b28efb6
Sven Bohm <bohms@msu.edu>
2010-02-15 15:55:12 -0500

    includes_dataset should return true if called without datatable

--- app/models/study.rb
+++ app/models/study.rb
@@ -3,6 +3,7 @@ class Study < ActiveRecord::Base
   has_and_belongs_to_many :datasets
   
   def include_datatables?(datatables = [])
+    return true if datatables.empty?
     datasets.collect  {|dataset| (dataset.datatables & datatables).any? }.include?(true)
   end
 end
--- test/unit/study_test.rb
+++ test/unit/study_test.rb
@@ -2,4 +2,25 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class StudyTest < ActiveSupport::TestCase
 
+  context 'querying for datatables' do
+    setup do
+      @study = Factory.create(:study)
+      @datatable  = Factory.create(:datatable)
+      @study.datasets << Factory.create(:dataset, :datatables => [@datatable])
+    end
+    
+    should 'return true if queried for any datatables' do
+      assert @study.include_datatables?
+    end
+
+    should 'return true if queried for included datatables' do
+      assert @study.include_datatables?([@datatable])
+    end
+    
+    should 'return false if queried for a non affiliated datatable' do
+      assert  !@study.include_datatables?([Factory.create(:datatable)])
+    end
+
+  end
+
 end
