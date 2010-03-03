38d7e702dba5ea5d8f9f74950e1b32c361e52fbe
Sven Bohm <bohms@msu.edu>
2010-03-03 07:35:42 -0500

    add created and updated fields to datatables and datasets

---
+++ db/migrate/20100303123318_add_created_and_updated.rb
@@ -0,0 +1,15 @@
+class AddCreatedAndUpdated < ActiveRecord::Migration
+  def self.up
+    add_column :datatables, :created_at, :datetime
+    add_column :datatables, :updated_at, :datetime
+    add_column :datasets, :created_at, :datetime
+    add_column :datasets, :updated_at, :datetime
+  end
+
+  def self.down
+    remove_column :datasets, :updated_at
+    remove_column :datasets, :created_at
+    remove_column :datatables, :updated_at
+    remove_column :datatables, :created_at
+  end
+end
