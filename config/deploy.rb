ef99ec13f4b538fa38ce692de060fab9fc83050d
Sven Bohm <bohms@msu.edu>
2010-03-25 12:44:07 -0400

    deploy from master

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :deploy_to, "/var/u/apps/#{application}"
 set :user, 'deploy'
 set :use_sudo, false
 
-set :branch, "searching"
+set :branch, "master"
 set :deploy_via, :copy
 #set :git_enable_submodules,1
 
