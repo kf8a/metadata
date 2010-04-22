afd74d2f6be5267ad162aed7dc4af6ab2c48e505
Sven Bohm <bohms@msu.edu>
2010-04-22 09:42:01 -0400

    upgrades

--- config/routes.rb
+++ config/routes.rb
@@ -34,6 +34,8 @@ ActionController::Routing::Routes.draw do |map|
   
   map.resources :studies
   
+  map.resources :data_contributions
+  
   map.root :controller => 'datasets'
 
   map.open_id_complete 'sessions', :controller => "sessions", :action => "create", :requirements => { :method => :get }
