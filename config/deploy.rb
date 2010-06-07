34a2198af84547fedd3f303014ed5d47748f1abc
Sven Bohm <bohms@msu.edu>
2010-06-07 12:00:58 -0400

    deploy to sebewa

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,8 @@ set :deploy_to, "/var/u/apps/#{application}"
 set :user, 'deploy'
 set :use_sudo, false
 
-set :branch, "clearance"
+set :branch, "upload"
+
 set :deploy_via, :copy
 #set :git_enable_submodules,1
 
