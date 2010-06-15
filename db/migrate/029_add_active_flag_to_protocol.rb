0817382265a1571df0ac0fe42d7f7a303c617582
Sven Bohm <bohms@msu.edu>
2010-06-15 15:11:40 -0400

    never created a migration for protocols

--- db/migrate/029_add_active_flag_to_protocol.rb
+++ db/migrate/029_add_active_flag_to_protocol.rb
@@ -1,9 +1,24 @@
 class AddActiveFlagToProtocol < ActiveRecord::Migration
   def self.up
-    add_column "protocols", "active", :boolean, :default=>true
+    create_table :table_name, :force => true do |t|
+      t.string  "name"
+      t.string  "title"
+      t.integer "version"
+      t.date    "in_use_from"
+      t.date    "in_use_to"
+      t.text    "description"
+      t.text    "abstract"
+      t.text    "body"
+      t.integer "person_id"
+      t.date    "created_on"
+      t.date    "updated_on"
+      t.text    "change_summary"
+      t.integer "dataset_id"
+      t.boolean "active",         :default => true
+    end
   end
 
   def self.down
-    remove_column "protocols", "active"
+    drop_table :table_name
   end
 end
