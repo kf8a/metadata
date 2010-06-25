8d771dc2668e6eda30d5a7f41f27e581251339b7
Sven Bohm <bohms@msu.edu>
2010-06-25 15:24:55 -0400

    add host parameter

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,7 @@ config.action_controller.perform_caching             = true
 # Enable serving of images, stylesheets, and javascripts from an asset server
 config.action_controller.asset_host                  = "http://lter.kbs.msu.edu/dynamic"
 
+config.action_mailer.default_url_options = {:host => 'sebewa.kbs.msu.edu'}
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
 
