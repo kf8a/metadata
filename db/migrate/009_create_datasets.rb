ac350c0f893cf8a223a8260557f6105603c77fca
Sven Bohm <bohms@msu.edu>
2010-07-12 09:47:00 -0400

    needed to add protocols

--- db/migrate/009_create_datasets.rb
+++ db/migrate/009_create_datasets.rb
@@ -12,6 +12,22 @@ class CreateDatasets < ActiveRecord::Migration
       t.column :on_web, :boolean, :default => true  
       t.column :version, :int
     end
+    
+    create_table "protocols", :force => true do |t|
+       t.string  "name"
+       t.string  "title"
+       t.integer "version"
+       t.date    "in_use_from"
+       t.date    "in_use_to"
+       t.text    "description"
+       t.text    "abstract"
+       t.text    "body"
+       t.integer "person_id"
+       t.date    "created_on"
+       t.date    "updated_on"
+       t.text    "change_summary"
+       t.integer "dataset_id"
+     end
   end
 
   def self.down
