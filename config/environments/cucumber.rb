1aa3a673c495a12e894e2b27aee4abc5181c3a21
Sven Bohm <bohms@msu.edu>
2010-07-14 11:12:48 -0400

    cucumber upgrade

--- config/environments/cucumber.rb
+++ config/environments/cucumber.rb
@@ -16,8 +16,6 @@ config.action_controller.perform_caching             = false
 # Disable request forgery protection in test environment
 config.action_controller.allow_forgery_protection    = false
 
-
-
 # Tell Action Mailer not to deliver emails to the real world.
 # The :test delivery method accumulates sent emails in the
 # ActionMailer::Base.deliveries array.
