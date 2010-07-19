117a7b164d0094b40876d221293c354dc4f6c256
Sven Bohm <bohms@msu.edu>
2010-07-19 09:08:34 -0400

    added roles to user

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100701150102) do
+ActiveRecord::Schema.define(:version => 20100719124908) do
 
   create_table "affiliations", :force => true do |t|
     t.integer "person_id"
@@ -426,6 +426,7 @@ ActiveRecord::Schema.define(:version => 20100701150102) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "identity_url"
+    t.string   "role"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email"
