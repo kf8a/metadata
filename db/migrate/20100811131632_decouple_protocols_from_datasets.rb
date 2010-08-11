9c6e78ff29e6e1dc8dc0bcf724628af3537813e4
Sven Bohm <bohms@msu.edu>
2010-08-11 14:36:36 -0400

    forgot the migration for the protocol decoupling

---
+++ db/migrate/20100811131632_decouple_protocols_from_datasets.rb
@@ -0,0 +1,17 @@
+class DecoupleProtocolsFromDatasets < ActiveRecord::Migration
+  def self.up
+    create_table :protocols_websites, :id => false, :force => true do |t|
+      t.integer :protocol_id
+      t.integer :website_id
+    end
+    create_table :datatables_protocols, :id => false, :force => true do |t|
+      t.integer :datatable_id
+      t.integer :protocol_id
+    end
+  end
+
+  def self.down
+    drop_table :datatables_protocols
+    drop_table :protocols_websites
+  end
+end
