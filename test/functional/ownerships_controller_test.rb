9fbf185f81b4a9aabfba8afa19080758d1ddd207
Jonathon D. Jones <jonathon_d_jones@yahoo.com>
2010-08-12 16:59:41 -0400

    Tests to make sure non-admin users don't have access to ownerships controller

--- test/functional/ownerships_controller_test.rb
+++ test/functional/ownerships_controller_test.rb
@@ -21,50 +21,22 @@ class OwnershipsControllerTest < ActionController::TestCase
         
         should respond_with :redirect
       end
+    end
       
-#      context "and GET :show the datatable's permissions" do
-#        setup do
-#          get :show, :id => @datatable
-#        end
-#        
-#        should_not respond_with :success
-#        should redirect_to("the permissions index") {permissions_path}
-#      end
-#    end
-#    
-#    context ", signed in as non-owner" do
-#      setup do
-#        @nonowner = Factory.create(:email_confirmed_user)
-#        @controller.current_user = @nonowner
-#      end
-#      
-#      context "and GET :new permission for the datatable" do
-#        setup do
-#          get :new, :datatable => @datatable
-#        end
-#        
-#        should_not respond_with :success
-#        should redirect_to("the permissions index") {permissions_path}
-#      end      
-#      
-#      context "and GET :show the datatable's permissions" do
-#        setup do
-#          get :show, :id => @datatable
-#        end
-#        
-#        should_not respond_with :success
-#        should redirect_to("the permissions index") {permissions_path}
-#      end
-#      
-#      context "and DELETE :destroy the datatable's permissions" do
-#        setup do
-#          delete :destroy, :id => @datatable
-#        end
-#        
-#        should_not respond_with :success
-#        should redirect_to("the permissions index") {permissions_path}
-#      end
-#    end
+    context ", signed in as non-admin" do
+      setup do
+        @nonadmin = Factory.create(:email_confirmed_user, :role => nil)
+        @controller.current_user = @nonadmin
+      end
+      
+      context "and GET :index" do
+        setup do
+          get :index
+        end
+        
+        should respond_with :redirect
+      end      
+    end
 #    
 #    context ", signed in as the owner" do
 #      setup do
@@ -146,6 +118,5 @@ class OwnershipsControllerTest < ActionController::TestCase
 #          should redirect_to("the datatable permission page") {permission_path(@datatable)}
 #        end
 #      end
-    end
   end
 end
