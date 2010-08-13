970622d1e657981a6fd6a449d0ecb66abc832bb6
Sven Bohm <bohms@msu.edu>
2010-08-12 21:51:56 -0400

    adds test for multiple website to protocl linkings

--- test/unit/protocol_test.rb
+++ test/unit/protocol_test.rb
@@ -6,15 +6,24 @@ class ProtocolTest < ActiveSupport::TestCase
   
   context 'a protocol' do
     setup do
-      @website = Factory.create(:website)
+      @website = Factory :website, :name=>'lter'
+      @website2 = Factory :website, :name => 'glbrc'
       @protocol = Factory.create(:protocol)
     end
     
     should 'save websites' do
-      @protocol.website_list = {@website.id.to_s => 1}
+      @protocol.website_list = {@website.id.to_s => '1', @website2.id.to_s => '0'}
       assert @protocol.save
       
       assert @protocol.websites.include?(@website)
+    end
+    
+    should 'save multiple websites' do
+       @protocol.website_list = {@website.id.to_s => '1', @website2.id.to_s => '1'}
+        assert @protocol.save
+
+        assert @protocol.websites.include?(@website)
+        assert @protocol.websites.include?(@website2)
     end
 
   end
