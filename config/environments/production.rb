872e3142caac5b78365425ada9431b0ed5ef8789
Sven Bohm <bohms@msu.edu>
2010-08-20 11:11:35 -0400

    set a more sensible return address

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,7 +14,8 @@ config.action_controller.perform_caching             = true
 # Enable serving of images, stylesheets, and javascripts from an asset server
 config.action_controller.asset_host                  = "http://sebewa.kbs.msu.edu/dynamic"
 
-config.action_mailer.default_url_options = {:host => 'sebewa.kbs.msu.edu'}
+config.action_mailer.default_url_options = {:host => 'glbrc.kbs.msu.edu'}
+config.mailer_sender = 'dontreply@lter.kbs.msu.edu'
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
 
