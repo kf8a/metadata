13d4c5e7c5ce2ba6e1c744348722ee2a820d8f48
Sven Bohm <bohms@msu.edu>
2010-08-20 11:29:27 -0400

    configured mail return addresses

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,6 @@ config.action_controller.perform_caching             = true
 config.action_controller.asset_host                  = "http://sebewa.kbs.msu.edu/dynamic"
 
 config.action_mailer.default_url_options = {:host => 'glbrc.kbs.msu.edu'}
-config.mailer_sender = 'dontreply@lter.kbs.msu.edu'
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
 
