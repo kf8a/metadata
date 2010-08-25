55914d6cfaeb9de3132f5111e491c14a9465b383
Sven Bohm <bohms@msu.edu>
2010-08-25 11:44:49 -0400

    makes the volume and page number display on citations smarter

--- app/helpers/citations_helper.rb
+++ app/helpers/citations_helper.rb
@@ -1,6 +1,17 @@
 module CitationsHelper
   def formatted_as_default(citation)
       authors = citation.authors.collect {|author| "#{author.sur_name} #{author.given_name}"}.join(', ')
-      "#{authors}. #{citation.pub_year}. <em>#{citation.title}</em> #{citation.publication} #{citation.volume}:#{citation.start_page_number}-#{citation.ending_page_number}"
+      volume_and_page = case
+        when citation.volume && citation.start_page_number && citation.ending_page_number
+          "#{citation.volume}:#{citation.start_page_number}-#{citation.ending_page_number}"
+        when citation.volume && citation.start_page_number
+          "#{citation.volume}:#{citation.start_page_number}"
+        when citation.volume
+          "#{citation.volume}"
+        else
+          ""
+        end
+      
+      "#{authors}. #{citation.pub_year}. <em>#{citation.title}</em> #{citation.publication} #{volume_and_page}"
   end
 end
--- test/unit/helpers/citations_helper_test.rb
+++ test/unit/helpers/citations_helper_test.rb
@@ -1,4 +1,12 @@
-require 'test_helper'
+require File.dirname(__FILE__) + '/../../test_helper'
 
 class CitationsHelperTest < ActionView::TestCase
+  
+  def setup
+    @citation = Citation.new
+  end
+  
+  def test_formatted_as_default
+    assert formatted_as_default(@citation).kind_of?(String)
+  end
 end
