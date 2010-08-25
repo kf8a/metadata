5955188d46cea0326ebefbba0d3b75b7cad1da8e
Sven Bohm <bohms@msu.edu>
2010-08-25 11:09:28 -0400

    formatted citation in citation helper and not in the model

--- app/models/citation.rb
+++ app/models/citation.rb
@@ -7,9 +7,5 @@ class Citation < ActiveRecord::Base
    :path => ":rails_root/assets/citations/:attachment/:id/:style/:basename.:extension"
   
   attr_protected :pdf_file_name, :pdf_content_type, :pdf_size
-  
-  def formatted_as_default
-    authors.collect {|author| "#{author.sur_name} #{author.given_name}"}.join(', ') + " <em> #{title.to_s}</em> #{pub_year.to_s}"
-  end
- 
+   
 end
