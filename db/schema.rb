2bbb7509485973e8b982d80d50617e6a450eaa04
Jonathon D. Jones <jonathon_d_jones@yahoo.com>
2010-08-28 23:23:50 -0400

    Creates a collections model

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100826133415) do
+ActiveRecord::Schema.define(:version => 20100829031613) do
 
   create_table "affiliations", :force => true do |t|
     t.integer "person_id"
@@ -67,6 +67,11 @@ ActiveRecord::Schema.define(:version => 20100826133415) do
     t.string   "pdf_content_type"
     t.integer  "pdf_file_size"
     t.datetime "pdf_updated_at"
+  end
+
+  create_table "collections", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "columns", :force => true do |t|
