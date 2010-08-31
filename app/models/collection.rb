a171452d7445b1d02177906f5cc47cfd474ae3e1
Jonathon D. Jones <jonathon_d_jones@yahoo.com>
2010-08-31 14:20:42 -0400

    Makes the collection query a full query without limits

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -2,4 +2,9 @@ class Collection < ActiveRecord::Base
   belongs_to :datatable
   
   validates_presence_of :datatable
+  
+  def perform_query
+    query =  self.datatable.object
+    ActiveRecord::Base.connection.execute(query)
+  end
 end
