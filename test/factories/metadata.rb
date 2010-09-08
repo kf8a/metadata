35c4cee23495a8e85598020a5c5804b34dc6a685
Jonathon D. Jones <jonathon_d_jones@yahoo.com>
2010-09-08 11:10:27 -0400

    Adds species to features, reorganizes factories

--- test/factories/metadata.rb
+++ test/factories/metadata.rb
@@ -1,14 +1,11 @@
-Factory.define :person do |p|
-  p.sur_name 'bauer'
-  p.given_name 'bill'
+#Independent factories#########
+Factory.define :affiliation do |affiliate|
 end
 
-Factory.define :role do |r|
-  r.name 'Emeritus Investigators'
+Factory.define :author do |author|
 end
 
-Factory.define :theme do |t|
-  t.name  'Agronomic'
+Factory.define :citation do |cite|
 end
 
 Factory.define :dataset do |d|
@@ -16,16 +13,15 @@ Factory.define :dataset do |d|
   d.abstract 'some new dataset'
 end
 
-Factory.define :protocol do |p|
-  p.name  'Proto1'
-  p.version  0
-  p.dataset Factory.create(:dataset)
+Factory.define :permission do |p|
 end
 
-Factory.define :sponsor do |s|
-  s.name    'LTER'
+Factory.define :person do |p|
+  p.sur_name 'bauer'
+  p.given_name 'bill'
 end
 
+<<<<<<< HEAD
 
 Factory.define :study do |s|
   s.name 'LTER'
@@ -41,19 +37,9 @@ Factory.define :datatable do |d|
   d.theme         Factory.create(:theme)
   d.dataset       Factory.create(:dataset)
   d.study         Factory.create(:study)
-end
-
-Factory.define :protected_datatable, :parent => :datatable do |datatable|
-  datatable.dataset   Factory.create(:dataset, :sponsor => Factory.create(:sponsor, :data_restricted => true))
-end
-
-
-Factory.define :website do |w|
-  w.name 'Name'
-end
-
-Factory.define :permission do |p|
-
+=======
+Factory.define :project do |p|
+>>>>>>> Adds species to features, reorganizes factories
 end
 
 Factory.define :publication do |p|
@@ -63,44 +49,78 @@ Factory.define :publication do |p|
   p.publication_type_id 1
 end
 
+Factory.define :role do |r|
+  r.name 'Emeritus Investigators'
+end
+
+Factory.define :species do |s|
+end
+
+Factory.define :sponsor do |s|
+  s.name    'LTER'
+end
+
+<<<<<<< HEAD
+=======
+Factory.define :study do |s|
+  s.name 'LTER'
+end
+
+Factory.define :unit do |u|
+end
+
+Factory.define :upload do |u|
+end
+
+>>>>>>> Adds species to features, reorganizes factories
 Factory.define :template do |t|
+end
+
+Factory.define :theme do |t|
+  t.name  'Agronomic'
 end
 
 Factory.define :variate do |v|
   v.name 'date'
 end
 
-Factory.define :upload do |u|
-end
-
 Factory.define :venue_type do |v|
   v.name  'Venue Name'
 end
 
+Factory.define :website do |w|
+  w.name 'Name'
+end
+
+#Dependent factories##########
+
+Factory.define :protocol do |p|
+  p.name  'Proto1'
+  p.version  0
+  p.dataset Factory.create(:dataset)
+end
+
+Factory.define :datatable do |d|
+  d.name          'KBS001_001'
+  d.title         'a really cool datatable'
+  d.object        'select now() as sample_date'
+  d.is_sql         true
+  d.description   'This is a datatable'
+  d.weight        100
+  d.theme         Factory.create(:theme)
+  d.dataset       Factory.create(:dataset)
+end
+
+Factory.define :protected_datatable, :parent => :datatable do |datatable|
+  datatable.dataset   Factory.create(:dataset, :sponsor => Factory.create(:sponsor, :data_restricted => true))
+end
+
+Factory.define :collection do |c|
+  c.datatable   Factory.create(:datatable)
+end
+
 Factory.define :meeting do |m|
   m.venue_type  Factory.create(:venue_type)
-end
-
-Factory.define :abstract do |a|
-  a.abstract  'A quick little discussion of the meeting.'
-  a.meeting   Factory.create(:meeting)
-end
-
-Factory.define :project do |p|
-end
-
-Factory.define :ownership do |o|
-#  o.user        Factory.create(:user)
-#  o.datatable   Factory.create(:datatable)
-end
-
-Factory.define :unit do |u|
-end
-
-Factory.define :affiliation do |affiliate|
-end
-
-Factory.define :citation do |cite|
 end
 
 Factory.define :author do |author|
@@ -109,4 +129,15 @@ end
 
 Factory.define :collection do |c|
   c.datatable   Factory.create(:datatable)
+
+Factory.define :abstract do |a|
+  a.abstract  'A quick little discussion of the meeting.'
+  a.meeting   Factory.create(:meeting)
 end
+
+Factory.define :ownership do |o|
+#  o.user        Factory.create(:user)
+#  o.datatable   Factory.create(:datatable)
+end
+
+
