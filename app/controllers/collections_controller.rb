aaf9d414a4e042cc4ff5e0c2479eaec848f9ac7c
Jonathon D. Jones <jonathon_d_jones@yahoo.com>
2010-09-09 12:04:45 -0400

    Now allows searching by a range in collections

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -12,13 +12,30 @@ class CollectionsController < ApplicationController
 
   def customize
     @collection = Collection.find(params[:id])
-    @limitby = params[:limitby]
-    @contains = params[:contains]
     @values = @collection.perform_query
+    @limitby = params[:limitby]
+    if @limitby == params[:oldlimitby] || params[:oldlimitby].blank?
+      @limit1 = params[:limit1]
+      @limit2 = params[:limit2]
+      @contains = params[:contains]
+      @limitrange = []
+    else
+      @limit1 = nil
+      @limit2 = nil
+      @contains = nil
+      @limitrange = []
+    end
+    @values.each do |row|
+      @limitrange << row[@limitby] if row[@limitby]
+    end
+    @limitrange.uniq!
+    @limitrange.sort!
     @limitoptions = []
     @values.fields.each do |field|
+      next if field == "id"
       @limitoptions << [field.titleize, field]
     end
+    @limitoptions.sort!
     @customize = params[:custom]
     render 'show'
   end
