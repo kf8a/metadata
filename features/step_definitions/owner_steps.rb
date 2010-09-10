ebcbc83251503574ee4f87c143453f9bb533caae
Jonathon D. Jones <jonathon_d_jones@yahoo.com>
2010-09-10 11:05:49 -0400

    Removes instance variables from owner_steps

--- features/step_definitions/owner_steps.rb
+++ features/step_definitions/owner_steps.rb
@@ -19,13 +19,13 @@ end
 
 Then /^"([^"]*)" should own the datatable "([^"]*)"$/ do |email, name|
   user = User.find_by_email(email)
-  @datatable = Datatable.find_by_name(name)
-  assert user.owns?(@datatable)
+  datatable = Datatable.find_by_name(name)
+  assert user.owns?(datatable)
 end
 
 Then /^"([^"]*)" should not own the datatable "([^"]*)"$/ do |email, name|
   user = User.find_by_email(email)
   user = Factory.create(:email_confirmed_user, :email => email) unless user
-  @datatable = Datatable.find_by_name(name)
-  assert !user.owns?(@datatable)
+  datatable = Datatable.find_by_name(name)
+  assert !user.owns?(datatable)
 end
