8afc232bfe31940651c148869a688f6063a06ec2
Sven Bohm <bohms@msu.edu>
2010-09-10 10:33:12 -0400

    test the path through the keyword display

--- test/functional/collections_controller_test.rb
+++ test/functional/collections_controller_test.rb
@@ -21,4 +21,14 @@ class CollectionsControllerTest < ActionController::TestCase
     should assign_to(:collection).with(@collection)
     should assign_to(:values)
   end
+  
+  context 'GET :show with keywords' do
+    setup do
+      @collection = Factory.create(:collection)
+      @collection.datatable.keyword_list = 'some words'
+      get :show, :id => @collection
+    end
+    
+    should respond_with :success
+  end
 end
