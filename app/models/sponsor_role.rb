677a70bfc536f270194fb6e340239b457edb5829
Sven Bohm <bohms@msu.edu>
2010-09-20 14:25:41 -0400

    do not use to_s(:long) it breaks in ruby 1.9

--- app/models/sponsor_role.rb
+++
@@ -1,4 +0,0 @@
-class SponsorRole < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :sponsor
-end
--- test/unit/sponsor_role_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class SponsorRoleTest < ActiveSupport::TestCase
-
-  should belong_to :sponsor
-  should belong_to :user
-  
-end
