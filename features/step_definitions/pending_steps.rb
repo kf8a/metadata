3e785d9d28be8f5ee93d0128c1363b7eab3f9f47
Jonathon D. Jones <jonathon_d_jones@yahoo.com>
2010-09-21 07:57:40 -0400

    Simplifies steps a little

--- features/step_definitions/pending_steps.rb
+++ features/step_definitions/pending_steps.rb
@@ -1,12 +1,3 @@
 Given /^a file "([^"]*)" has been uploaded$/ do |arg1|
   pending # express the regexp above with the code you wish you had
 end
-
-Given /^all caches are cleared$/ do
-  pending # express the regexp above with the code you wish you had
-end
-
-Then /^I should be signed in$/ do
-  pending # express the regexp above with the code you wish you had
-end
-
