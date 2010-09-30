cf985ff9578a45cf580cae16df2ae76cd3720ec9
Sven Bohm <bohms@msu.edu>
2010-09-30 12:28:52 -0400

    customize the confirmation email

--- app/models/invite_mailer.rb
+++ app/models/invite_mailer.rb
@@ -1,7 +1,7 @@
 class InviteMailer < ActionMailer::Base
   
   def invitation(invite)
-    from       "sippel@kbs.msu.edu"
+    from       "sippel@kbs.msu.edu <Suzanne Sippel>"
     recipients invite.email
     subject    "Welcome to the GLBRC Sustainability Data Catalog"
     body       :invite => invite
