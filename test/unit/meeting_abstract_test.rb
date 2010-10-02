c1666cbad717792ffa75475832c43dc31a524741
Jonathon D. Jones <jonathon_d_jones@yahoo.com>
2010-10-02 18:19:13 -0400

    Standardized requires for test_helper

--- test/unit/meeting_abstract_test.rb
+++ test/unit/meeting_abstract_test.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/../test_helper'
+require 'test_helper'
 
 class MeetingAbstractTest < ActiveSupport::TestCase
   fixtures :meeting_abstracts
--- test/unit/upload_test.rb
+++ test/unit/upload_test.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/../test_helper'
+require 'test_helper'
 
 class UploadTest < ActiveSupport::TestCase
   context 'saving' do 
