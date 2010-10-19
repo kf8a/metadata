433615783f618bb4109480e27c6e4c58ddb2e19e
Jonathon Jones <jonathon_d_jones@yahoo.com>
2010-10-19 19:46:12 -0400

    Adds back in the footer of environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Metadata::Application.initialize!
+
+#Maybe these belong here
+SubdomainFu.tld_sizes = {:development => 0,
+                         :cucumber => 0,
+                         :test => 0,
+                         :production => 3} # set all at once (also the defaults)
+
+
+OpenIdAuthentication.store = :file
+
+ActionController::Base.cache_store = :file_store, "tmp/cache" #"/path/to/cache/directory"
+Struct.new('Crumb', :url, :name)
