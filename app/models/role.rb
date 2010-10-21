2ffc0798afc1def54b2cec4f232318b012fe6b98
Jonathon Jones <jonathon_d_jones@yahoo.com>
2010-10-21 19:02:33 -0400

    Fixes roles/role_types

--- app/models/role.rb
+++ app/models/role.rb
@@ -3,14 +3,16 @@ class Role < ActiveRecord::Base
   has_many :affiliations
   belongs_to :role_type
   
-  scope :data_roles, :conditions => ["role_type_id = ?", RoleType.find_by_name('lter_dataset')]
-
   def emeritus?
     name =~ /^Emeritus/
   end
   
   def committee?
     name =~ /Committee/ || name =~/Network Representatives/
+  end
+
+  def self.data_roles
+    self.where(:role_type_id => RoleType.find_by_name('lter_dataset'))
   end
   
 end
