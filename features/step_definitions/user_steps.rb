494c56d195134470065e66e6bfce956ea6c99c37
Jonathon Jones <jonathon_d_jones@yahoo.com>
2010-10-26 17:36:39 -0400

    Deleted the steps which are already done in clearance

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,23 +1,3 @@
-# Database
-
-Given /^no user exists with an email of "(.*)"$/ do |email|
-  assert_nil User.find_by_email(email)
-end
-
-Given /^I signed up with "(.*)"\/"(.*)"$/ do |email, password|
-  Factory :user,
-    :email                 => email,
-    :password              => password,
-    :password_confirmation => password
-end
-
-Given /^I am signed up and confirmed as "(.*)"\/"(.*)"$/ do |email, password|
-  Factory :email_confirmed_user,
-    :email                 => email,
-    :password              => password,
-    :password_confirmation => password
-end
-
 Given /^a user exists and is confirmed with an email of "([^"]*)"$/ do |email|
   Factory :email_confirmed_user, :email => email
 end
