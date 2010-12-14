0b6296aa776b4b500935ae5b92dee2b4dea6f70b
Sven Bohm <bohms@msu.edu>
2010-12-14 13:47:02 -0500

    use lter.kbs.msu.edu as the asset host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ Metadata::Application.configure do
   config.serve_static_assets = false
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = "http://lter.kbs.msu.edu"
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
