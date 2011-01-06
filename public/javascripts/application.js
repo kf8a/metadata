e2d1d992aa987bede53eed1a5cff31bad6ce150d
Sven Bohm <bohms@msu.edu>
2011-01-06 11:51:25 -0500

    added author removal as per Ryans screencast

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -30,3 +30,14 @@ jQuery(document).ready(function() {
 			jQuery(this).text('[Expand All]');
 		});
 	});
+
+  function remove_fields(link) {
+      jQuery(link).prev("input[type=hidden]").val("1");
+        jQuery(link).parent().parent(".inputs").hide();
+  }
+
+  function add_fields(link, association, content) {
+      var new_id = new Date().getTime();
+        var regexp = new RegExp("new_" + association, "g")
+          jQuery(link).parent().before(content.replace(regexp, new_id));
+  }
