58af3ead28ed24d7779bf41ea478ab01951bfa42
Jonathon Jones <jonathon_d_jones@yahoo.com>
2011-01-06 15:15:43 -0500

    Bring back subdomain steps

--- features/step_definitions/subdomains_steps.rb
+++ features/step_definitions/subdomains_steps.rb
@@ -1,7 +1,7 @@
 Given /^I am in the (.*) subdomain$/ do |subdomain|
-#  subdomain = subdomain.downcase
-#  Capybara.default_host = "#{subdomain}.localhost"
-#  Capybara.app_host = "http://#{subdomain}.localhost:3000"
+  subdomain = subdomain.downcase
+  Capybara.default_host = "#{subdomain}.localhost"
+  Capybara.app_host = "http://#{subdomain}.localhost:3000"
 end
 
 When /^I follow the redirect$/ do
