22c1687e02ce6137ffc1acb877bfcae7e542a21b
Sven Bohm <bohms@msu.edu>
2011-01-07 13:49:18 -0500

    have the authors format themselves

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,4 +2,32 @@ class Author < ActiveRecord::Base
   belongs_to :citation
   
   validates_presence_of :seniority
+
+  def formatted(option = :default)
+    if option == :natural
+      format_as_natural
+    else 
+      format_as_default
+    end
+  end
+  
+  def format_as_default
+    if given_name and middle_name
+      "#{sur_name.capitalize}, #{given_name[0].upcase}. #{middle_name[0].upcase}."
+    elsif given_name
+      "#{sur_name.capitalize}, #{given_name[0].upcase}."
+    else
+      sur_name.capitalize
+    end
+  end
+
+  def format_as_natural
+    if given_name and middle_name
+      "#{given_name[0].upcase}. #{middle_name[0].upcase}. #{sur_name.capitalize}"
+    elsif given_name
+      "#{given_name[0].upcase}. #{sur_name.capitalize}"
+    else
+      sur_name.capitalize
+    end
+  end
 end
--- test/unit/author_test.rb
+++ test/unit/author_test.rb
@@ -4,4 +4,49 @@ class AuthorTest < ActiveSupport::TestCase
 
   should belong_to :citation
   should validate_presence_of 'seniority'
+
+  context 'an author with a last name' do
+    setup do
+      @author = Factory :author, :sur_name => 'bond'
+    end
+
+    should 'be formatted correctly as default' do
+      assert_equal 'Bond', @author.formatted
+    end
+
+    should 'be formatted corretly as natural' do
+      assert_equal 'Bond', @author.formatted(:natural)
+    end
+  end
+
+  context 'an author with a first and last name' do
+    setup do
+      @author1 = Factory :author, :sur_name => 'Bond', :given_name => 'Bill'
+      @author2 = Factory :author, :sur_name => 'Bond', :given_name => 'B.'
+    end
+
+    should 'be formatted correctly as default' do
+      assert_equal 'Bond, B.', @author1.formatted
+      assert_equal 'Bond, B.', @author2.formatted
+    end
+
+    should 'be formatted correctly as natural' do
+      assert_equal 'B. Bond', @author1.formatted(:natural)
+      assert_equal 'B. Bond', @author2.formatted(:natural)
+    end
+  end
+
+  context 'an author with a middle name' do
+    setup do
+      @author1 = Factory :author, :sur_name => 'Bond', :given_name => 'Bill', :middle_name => 'karl'
+    end
+
+    should 'be formatted correctly as default' do
+      assert_equal 'Bond, B. K.', @author1.formatted
+    end
+
+    should 'be formatted correctly as natural' do
+      assert_equal 'B. K. Bond', @author1.formatted(:natural)
+    end
+  end
 end
