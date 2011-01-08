c1a85e2f2a20e12d077778e8cd59b2d61472c3a6
Sven Bohm <bohms@msu.edu>
2011-01-08 09:07:31 -0500

    moved the citation format into a module so I can use it in both author and editor

--- app/models/editor.rb
+++ app/models/editor.rb
@@ -1,4 +1,8 @@
+require 'citation_format'
+
 class Editor < ActiveRecord::Base
+  include CitationFormat
+
   belongs_to :citation
 
   validates_presence_of :seniority
---
+++ lib/citation_format.rb
@@ -0,0 +1,39 @@
+module CitationFormat
+  def formatted(option = :default)
+    if option == :natural
+      format_as_natural
+    else 
+      format_as_default
+    end
+  end
+  
+  def format_as_default
+    if has_given_name? and  has_middle_name?
+      "#{sur_name.capitalize}, #{given_name[0].upcase}. #{middle_name[0].upcase}."
+    elsif has_given_name?
+      "#{sur_name.capitalize}, #{given_name[0].upcase}."
+    else
+      sur_name.capitalize
+    end
+  end
+
+  def format_as_natural
+    if has_given_name? and has_middle_name?
+      "#{given_name[0].upcase}. #{middle_name[0].upcase}. #{sur_name.capitalize}"
+    elsif given_name
+      "#{given_name[0].upcase}. #{sur_name.capitalize}"
+    else
+      sur_name.capitalize
+    end
+  end
+
+  private
+  
+  def has_given_name?
+    !(given_name.nil? || given_name.try(:empty?))
+  end
+
+  def has_middle_name?
+    !(middle_name.nil? || middle_name.try(:empty?))
+  end
+end
