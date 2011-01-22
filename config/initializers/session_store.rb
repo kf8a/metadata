cfde9d3e2a39b98b8b09765d37b459486eccbb85
Sven Bohm <bohms@msu.edu>
2011-01-21 21:04:53 -0500

    no need to use the domain option as long as i do not use the _url helpers

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -7,5 +7,5 @@ else
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rake db:sessions:create")
-  Metadata::Application.config.session_store :active_record_store, :domain => :all
+  Metadata::Application.config.session_store :active_record_store #, :domain => :all
 end
