213666a6749e796b01d1772aa35712c430fa9021
Sven Bohm <bohms@msu.edu>
2011-01-26 19:00:36 -0500

    space cleanup

--- test/unit/protocol_test.rb
+++ test/unit/protocol_test.rb
@@ -3,7 +3,7 @@ require File.expand_path('../../test_helper',__FILE__)
 class ProtocolTest < ActiveSupport::TestCase
   should have_and_belong_to_many :websites
   should have_and_belong_to_many :datatables
-  
+
   context 'a protocol' do
     setup do
       @website = Factory :website, :name=>'lter'
@@ -11,14 +11,14 @@ class ProtocolTest < ActiveSupport::TestCase
       @protocol = Factory.create(:protocol)
       @other_protocol = Factory.create(:protocol)
     end
-    
+
     should 'save websites' do
       @protocol.website_list = {@website.id.to_s => '1', @website2.id.to_s => '0'}
       assert @protocol.save
-      
+
       assert @protocol.websites.include?(@website)
     end
-    
+
     should 'save multiple websites' do
        @protocol.website_list = {@website.id.to_s => '1', @website2.id.to_s => '1'}
         assert @protocol.save
