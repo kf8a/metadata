8e50be7f00cbe64bb63612c145fca9e866e4671c
Sven Bohm <bohms@msu.edu>
2011-01-28 12:48:04 -0500

    simplified protocol steps by using web_steps

--- features/step_definitions/protocol_steps.rb
+++
@@ -1,9 +0,0 @@
-When(/^I check the "(.+?)" website$/) do |name|
-  @website = Website.find_by_name(name)
-  check "protocol[website_list][#{@website.id}]"
-end
-
-When(/^I uncheck the "(.+?)" website$/) do |name|
-  @website = Website.find_by_name(name)
-  uncheck "protocol[website_list][#{@website.id}]"
-end
