0df3a5fa4a62107a629387f21a82fb19cd173968
Jonathon D. Jones <jonathon_d_jones@yahoo.com>
2011-02-21 17:27:16 -0500

    Better test for Dataset#update_temporal_extent

--- test/unit/dataset_test.rb
+++ test/unit/dataset_test.rb
@@ -1,15 +1,15 @@
-require File.expand_path('../../test_helper',__FILE__) 
+require File.expand_path('../../test_helper',__FILE__)
 
 class DatasetTest < ActiveSupport::TestCase
-    
+
   should have_many :datatables
   should have_many :affiliations
   should belong_to :website
-  
+
   should have_and_belong_to_many :themes
   should have_and_belong_to_many :studies
-  
-  
+
+
   context 'dataset' do
     setup do
       @dataset = Factory.create(:dataset)
@@ -90,63 +90,64 @@ class DatasetTest < ActiveSupport::TestCase
 
   context 'no temporal extent' do
     setup do
-      @dataset = Factory.create(:dataset, :initiated => '2000-1-1', 
+      @dataset = Factory.create(:dataset, :initiated => '2000-1-1',
         :datatables => [Factory.create(:datatable, :object => 'select 1')])
     end
-    
+
     should 'not update temporal extent if extent is missing' do
       @dataset.update_temporal_extent
       assert @dataset.initiated == Date.parse('2000-1-1')
       assert @dataset.completed == nil
     end
   end
-   
+
   context 'Current Temporal Extent' do
     setup do
-      @dataset = Factory.create(:dataset, 
-        :datatables  => [Factory.create(:datatable, :object => 'select 1'), 
-                         Factory.create(:datatable, 
+      @dataset = Factory.create(:dataset,
+        :datatables  => [Factory.create(:datatable, :object => 'select 1'),
+                         Factory.create(:datatable,
                          :object => "select now() as sample_date"),
                          Factory.create(:datatable, :object => 'select 1')])
     end
-  
+
     should 'be today' do
       dates = @dataset.temporal_extent
       assert  dates[:begin_date] == Date.today
       assert dates[:end_date] == Date.today
     end
-    
+
     should 'update temporal extent to today' do
       @dataset.update_temporal_extent
+      @dataset.reload #make sure it updates in the database
       assert @dataset.initiated == Date.today
       assert @dataset.completed == Date.today
     end
 
   end
-  
+
   context 'past temporal extent' do
     setup do
-      @dataset = Factory.create(:dataset, 
-        :datatables  => [Factory.create(:datatable, :object => 'select now() as sample_date'), 
-                         Factory.create(:datatable, 
+      @dataset = Factory.create(:dataset,
+        :datatables  => [Factory.create(:datatable, :object => 'select now() as sample_date'),
+                         Factory.create(:datatable,
                          :object => "select now() - interval '1 year' as sample_date")])
     end
-    
+
     should 'be today to a year ago' do
       dates = @dataset.temporal_extent
       assert  dates[:begin_date] == Date.today - 1.year
       assert dates[:end_date] == Date.today
     end
-    
-    
+
+
   end
-  
+
   context 'eml generatation' do
-    setup do 
+    setup do
       @dataset = Factory.create(:dataset, :initiated => Date.today, :completed => Date.today)
-      @dataset_with_datatable = Factory.create(:dataset, :datatables  => [Factory.create(:datatable),  Factory.create(:datatable)])    
+      @dataset_with_datatable = Factory.create(:dataset, :datatables  => [Factory.create(:datatable),  Factory.create(:datatable)])
     end
-    
+
     should 'be successful' do
       assert !@dataset.to_eml.nil?
       assert !@dataset_with_datatable.to_eml.nil?
@@ -158,15 +159,15 @@ class DatasetTest < ActiveSupport::TestCase
         @dataset.protocols << protocol
       end
 
-      should 'have a methods section' 
+      should 'have a methods section'
     end
   end
-  
+
   context 'set affiliations' do
     setup do
       @dataset = Factory.create(:dataset)
     end
-    
+
     should 'be able to add a affiliation when none exists' do
       params = { :dataset=>{"title"=>"Trace Gas Fluxes on the Main Cropping System Experiment",
        "abstract"=>"Trace gases (methane,
@@ -196,30 +197,30 @@ class DatasetTest < ActiveSupport::TestCase
        "seniority"=>"1"}]},
        "commit"=>"Update",
        "id"=>"16"}
-       
+
       assert @dataset.update_attributes(params[:dataset])
     end
   end
-  
+
   context "within_interval? function" do
     setup do
       @dataset = Factory.create(:dataset)
     end
-    
+
     context "and two datatables" do
       setup do
-        Factory.create(:datatable, :dataset => @dataset, 
-          :object => 'select now() as sample_date',  
+        Factory.create(:datatable, :dataset => @dataset,
+          :object => 'select now() as sample_date',
           :begin_date => (Date.today - 7),   :end_date => (Date.today - 4))
-        Factory.create(:datatable, :dataset => @dataset, 
-          :object => %q{select now() - interval '1 year' as sample_date}, 
+        Factory.create(:datatable, :dataset => @dataset,
+          :object => %q{select now() - interval '1 year' as sample_date},
           :begin_date => (Date.today - 365), :end_date => (Date.today - 364))
       end
-      
+
       should "return true for dates which include a datatable" do
         assert @dataset.within_interval?(Date.today - 10, Date.today)
       end
-      
+
       should "return false for dates which do not include a datatable" do
         assert !@dataset.within_interval?(Date.today - 500, Date.today - 400)
       end
