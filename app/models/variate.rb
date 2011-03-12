b67a8c7e2e8699752235009ce5f87466d9972749
Jonathon D. Jones <jonathon_d_jones@yahoo.com>
2011-03-12 08:49:04 -0500

    Add tests to variate model

--- app/models/variate.rb
+++ app/models/variate.rb
@@ -4,7 +4,7 @@ include REXML
 class Variate < ActiveRecord::Base
   belongs_to :datatable
   belongs_to :unit
-  
+
   def to_eml
     eml = Element.new('attribute')
     eml.add_element('attributeName').add_text(self.name)
@@ -20,36 +20,36 @@ private
     case self.measurement_scale
     when 'interval' then eml_interval(scale)
     when 'ratio' then eml_ratio(scale)
-    when 'ordinal' then 
+    when 'ordinal' then
     when 'nominal'then  eml_nominal(scale)
     when 'dateTime' then eml_date_time(scale)
     end
     m
   end
-  
+
   def eml_unit
     u = Element.new('unit')
-    if self.unit.in_eml
+    if self.unit.try(:in_eml)
       u.add_element('standardUnit').add_text(self.unit.name)
     else
-      u.add_element('customUnit').add_text(self.unit.name)
+      u.add_element('customUnit').add_text(self.unit.try(:name))
       # TODO: need to add custom unit tags to the end of the document
     end
     u
   end
-  
+
   def eml_date_time(e)
     e.add_element('formatString').add_text(self.date_format)
     e.add_element('dateTimePrecision').add_text('86400')
     e.add_element('dateTimeDomain').add_element('bounds') \
       .add_element('minimum', {'exclusive' => 'true'}).add_text('1987-4-18')
-    
+
   end
-  
+
   def eml_nominal(e)
     e.add_element('nonNumericDomain').add_element('textDomain').add_element('definition').add_text(self.description)
   end
-  
+
   def eml_interval(e)
     e.add_element eml_unit
     if !self.precision
@@ -60,7 +60,7 @@ private
     end
     e.add_element('numericDomain').add_element('numberType').add_text(self.data_type)
   end
-  
+
   def eml_ratio(e)
     e.add_element('unit')
     if !self.precision
--- test/unit/variate_test.rb
+++ test/unit/variate_test.rb
@@ -1,5 +1,119 @@
-require File.expand_path('../../test_helper',__FILE__) 
+require File.expand_path('../../test_helper',__FILE__)
 
 class VariateTest < ActiveSupport::TestCase
 
+  should belong_to :datatable
+  should belong_to :unit
+
+  context 'A variate' do
+    setup do
+      @variate = Factory :variate
+    end
+
+    context '#to_eml' do
+      setup do
+        @to_eml = @variate.to_eml.to_s
+      end
+
+      should 'be successful' do
+        assert_equal "<attribute><attributeName>date</attributeName><attributeDefinition/></attribute>", @to_eml
+      end
+    end
+  end
+
+  context 'A variate with a description' do
+    setup do
+      @variate = Factory :variate, :description => 'A description'
+    end
+
+    context '#to_eml' do
+      setup do
+        @to_eml = @variate.to_eml.to_s
+      end
+
+      should 'include that description in attributeDefinition tags' do
+        assert @to_eml.include?('<attributeDefinition>A description</attributeDefinition>')
+      end
+    end
+  end
+
+  context 'A variate with an interval measurement scale' do
+    setup do
+      @variate = Factory :variate, :measurement_scale => 'interval'
+    end
+
+    context '#to_eml' do
+      setup do
+        @to_eml = @variate.to_eml.to_s
+      end
+
+      should 'include the scale' do
+        assert @to_eml.include?('<measurementScale><interval><unit><customUnit/></unit><precision>1<!--default precision none specified--></precision><numericDomain><numberType/></numericDomain></interval></measurementScale>')
+      end
+    end
+  end
+
+  context 'A variate with a ratio measurement scale' do
+    setup do
+      @variate = Factory :variate, :measurement_scale => 'ratio'
+    end
+
+    context '#to_eml' do
+      setup do
+        @to_eml = @variate.to_eml.to_s
+      end
+
+      should 'include the scale' do
+        assert @to_eml.include?('<measurementScale><ratio><unit/><precision>1<!--default precision none specified--></precision><numericDomain><numberType/></numericDomain></ratio></measurementScale>')
+      end
+    end
+  end
+
+  context 'A variate with an ordinal measurement scale' do
+    setup do
+      @variate = Factory :variate, :measurement_scale => 'ordinal'
+    end
+
+    context '#to_eml' do
+      setup do
+        @to_eml = @variate.to_eml.to_s
+      end
+
+    should 'include the scale' do
+        assert @to_eml.include?('<measurementScale><ordinal/></measurementScale>')
+      end
+    end
+  end
+
+  context 'A variate with a nominal measurement scale' do
+    setup do
+      @variate = Factory :variate, :measurement_scale => 'nominal'
+    end
+
+    context '#to_eml' do
+      setup do
+        @to_eml = @variate.to_eml.to_s
+      end
+
+      should 'include the scale' do
+        assert @to_eml.include?('<attributeDefinition/><measurementScale><nominal><nonNumericDomain><textDomain><definition/></textDomain></nonNumericDomain></nominal></measurementScale>')
+      end
+    end
+  end
+
+  context 'A variate with a date time measurement scale' do
+    setup do
+      @variate = Factory :variate, :measurement_scale => 'dateTime'
+    end
+
+    context '#to_eml' do
+      setup do
+        @to_eml = @variate.to_eml.to_s
+      end
+
+      should 'include the scale' do
+        assert @to_eml.include?("<measurementScale><dateTime><formatString/><dateTimePrecision>86400</dateTimePrecision><dateTimeDomain><bounds><minimum exclusive='true'>1987-4-18</minimum></bounds></dateTimeDomain></dateTime></measurementScale>")
+      end
+    end
+  end
 end
