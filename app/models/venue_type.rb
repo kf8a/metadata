d83c48a52ba5729bca94786aaafa8b5de7f166c3
Jonathon D. Jones <jonathon_d_jones@yahoo.com>
2011-03-11 20:45:25 -0500

    Adds description to VenueType model

--- app/models/venue_type.rb
+++ app/models/venue_type.rb
@@ -1,5 +1,6 @@
+# Meetings can be in local, national, or other types of venues.
 class VenueType < ActiveRecord::Base
   has_many :meetings, :order => 'date desc'
-  
+
   validates_presence_of :name
 end
