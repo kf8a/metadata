9ebc7db48615e536a57b2702a5fd2a7618e0dff2
Jonathon D. Jones <jonathon_d_jones@yahoo.com>
2011-03-28 23:31:13 -0400

    Makes a test for Permission.permitted_users

--- test/unit/permission_test.rb
+++ test/unit/permission_test.rb
@@ -1,4 +1,4 @@
-require File.expand_path('../../test_helper',__FILE__) 
+require File.expand_path('../../test_helper',__FILE__)
 
 class PermissionTest < ActiveSupport::TestCase
   should belong_to :user
@@ -9,14 +9,14 @@ class PermissionTest < ActiveSupport::TestCase
   should validate_presence_of :user
   should validate_presence_of :datatable
   should validate_presence_of :owner
-  
+
   # TODO figure out how to write a matcher for associated models
   # should_validate_associated :user
   # should_validate_associated :datatable
   # should_validate_associated :owner
 
   context 'basic permissions' do
-    setup do 
+    setup do
       user        = Factory :user
       owner       = Factory :user
       datatable   = Factory :datatable, :owners => [owner]
@@ -30,7 +30,7 @@ class PermissionTest < ActiveSupport::TestCase
     should 'be valid' do
       assert @permission.valid?
     end
-    
+
   end
 
   context 'setting permissions' do
@@ -43,7 +43,7 @@ class PermissionTest < ActiveSupport::TestCase
       p           = Permission.new({:datatable => @datatable})
       p.owner     = @owner
       p.user      = Factory :user
-      
+
       assert p.valid?
     end
 
@@ -52,24 +52,24 @@ class PermissionTest < ActiveSupport::TestCase
       p.datatable = @datatable
       p.owner     = Factory :user
       p.user      = Factory :user
-      
+
       assert !p.valid?
     end
 
     should 'not allow permissions to be set more than once' do
       user         = Factory :user
-      
+
       p1           = Permission.new
       p1.datatable = @datatable
       p1.owner     = @owner
       p1.user      = user
-      
-      
+
+
       p2           = Permission.new
       p2.datatable = @datatable
       p2.owner     = @owner
       p2.user      = user
-       
+
       #TODO after saving the permission becomes invalid. Don't know if that would cause problems elsewere.
       assert p1.valid?
       assert p1.save
@@ -112,6 +112,27 @@ class PermissionTest < ActiveSupport::TestCase
 
       assert p2.valid?
       assert p2.save
+    end
+  end
+
+  context 'Person.permitted_users' do
+    should 'list all undenied users' do
+      user1 = Factory :user
+      user2 = Factory :user
+      user3 = Factory :user
+      user4 = Factory :user
+      owner1 = Factory :user
+      owner2 = Factory :user
+      datatable1  = Factory :datatable, :owners => [owner1]
+      datatable2  = Factory :datatable, :owners => [owner2]
+      Factory :permission, :owner => owner1, :datatable => datatable1, :user => user1
+      Factory :permission, :owner => owner1, :datatable => datatable1, :user => user2, :decision => 'denied'
+      Factory :permission, :owner => owner2, :datatable => datatable2, :user => user4
+
+      assert Permission.permitted_users.include?(user1)
+      assert !Permission.permitted_users.include?(user2)
+      assert !Permission.permitted_users.include?(user3)
+      assert Permission.permitted_users.include?(user4)
     end
   end
 end
