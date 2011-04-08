b61ce42db6c5aad6903fcc9821432b7713d2f861
Sven Bohm <bohms@msu.edu>
2011-04-08 14:37:23 -0600

    added link for bibtex export and trying to use proper mime types.

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -10,7 +10,7 @@
 # Mime::Type.register "application/x-mobile", :mobile
 Mime::Type.register "text/xml", :eml
 Mime::Type.register "text/csv", :climdb
-Mime::Type.register "text/text", :enw
-Mime::Type.register "text/text", :bib
+Mime::Type.register "application/x-endnote-refer", :enw
+Mime::Type.register "application/x-bibtex", :bib
 #Mime::Type.register "application/vnd.ms-excel", :xls
 
