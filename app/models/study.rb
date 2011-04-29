76b67d4a9469f0c2c984643e4822ce04b5fadcb7
Jonathon Jones <jonathon_d_jones@yahoo.com>
2011-04-28 21:16:25 -0400

    Simplify Study#self.find_all_roots_with_datatables

--- app/models/study.rb
+++ app/models/study.rb
@@ -24,7 +24,7 @@ class Study < ActiveRecord::Base
   end
 
   def self.find_all_roots_with_datatables(tables=[])
-    self.roots.order('weight').collect {|x| x.include_datatables?(tables) ? x : nil}.compact
+    self.roots.order('weight').keep_if {|study| study.include_datatables?(tables) }
   end
 
   private
