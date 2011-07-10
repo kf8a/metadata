608bb67e3f2f99113fa5fa0278eab99400946111
Jonathon Jones <jonathon_d_jones@yahoo.com>
2011-07-10 19:20:44 -0400

    Simplifies Userscontroller#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,11 @@
 class UsersController < Clearance::UsersController
-  
+
   def create
     @user = User.new params[:user]
-    invite_code = params[:invite_code]
-    @invite = Invite.find_redeemable(invite_code)
+    @invite = Invite.find_redeemable(params[:invite_code])
 
     if @user.save
-      if invite_code && @invite
+      if @invite
         @invite.redeemed!
         if @invite.glbrc_member?
           @user.sponsors << Sponsor.find_by_name('glbrc')
@@ -17,7 +16,7 @@ class UsersController < Clearance::UsersController
       flash_notice_after_create
       redirect_to(url_after_create)
     else
-      render 'users/new'
+      render :new
     end
   end
 
