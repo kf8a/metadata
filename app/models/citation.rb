da79d33122ffcda069fcb82784ad06a80ad25ad1
Jonathon Jones <jonathon_d_jones@yahoo.com>
2011-07-17 22:50:10 -0400

    Remove commented out code

--- app/models/citation.rb
+++ app/models/citation.rb
@@ -146,25 +146,6 @@ class Citation < ActiveRecord::Base
         citation.authors.create(:name => author_name, :seniority => index)
       end
       citation
-
-      #parser = RISParser.new
-      #
-      #parser.consume_all_input = false
-      #file = File.open(args[:file],"r:UTF-8")
-      #content = file.read
-      #a = parser.parse(content[1..-1])
-      #p parser.failure_reason
-      #path = Pathname.new(File.dirname(file))
-      #citations = a.content(path.realpath.to_s + '/' + File.basename(file, '.ris').to_s + '.Data/PDF/')
-      #
-      #p citations
-      #web = Website.find_by_name('lter')
-      #citations.flatten.each do |citation|
-      #  citation.website = web
-      #  p citation
-      #  p citation.save
-      #  p citation.errors
-      #end
     end
   end
 
