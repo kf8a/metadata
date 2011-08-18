f800839ef21b610ca95470c75a8d4e3f9e3f4aef
Jonathon Jones <jonathon_d_jones@yahoo.com>
2011-08-17 23:48:34 -0400

    Adds a helpful message when the dataset fails to import

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -84,6 +84,10 @@ class DatasetsController < ApplicationController
   def create
     if params[:eml_link].present?
       @dataset = Dataset.from_eml(params[:eml_link])
+      unless @dataset.class == Dataset #if not a Dataset, it will be an array of errors
+        flash[:notice] = "Eml import had errors: " + @dataset.collect{|error| error.to_s}.join(' ')
+        @dataset = Dataset.new
+      end
     elsif params[:dataset][:eml_file].present?
       @dataset = Dataset.from_eml_file(params[:dataset][:eml_file])
     else
