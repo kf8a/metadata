73ea7350acf03f4c32a75724f19ac022f640d3be
Sven Bohm <bohms@msu.edu>
2011-09-12 15:27:24 -0400

    also expire the climdb version

--- app/controllers/datatable_sweeper.rb
+++ app/controllers/datatable_sweeper.rb
@@ -24,6 +24,7 @@ class DatatableSweeper < ActionController::Caching::Sweeper
     expire_fragment(:controller => 'datatables', :action => 'show',  :action_suffix => 'page', :id => datatable)
     expire_fragment(:controller => 'datatables', :action => 'show',  :action_suffix => 'data', :id => datatable)
     expire_action(:controller => 'datatables', :action => 'show', :id => datatable, :format => 'csv')
+    expire_action(:controller => 'datatables', :action => 'show', :id => datatable, :format => 'climdb')
     expire_csv_cache(datatable)
   end
 
