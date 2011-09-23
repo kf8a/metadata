bd79898a9a2034230483ccf8a41111a56eaecfa3
Sven Bohm <bohms@msu.edu>
2011-09-23 10:27:10 -0400

    added caching for the individual elements in the citation page

--- app/controllers/citation_sweeper.rb
+++ app/controllers/citation_sweeper.rb
@@ -17,6 +17,7 @@ class CitationSweeper < ActionController::Caching::Sweeper
 
   def expire_cache_for(citation)
     expire_fragment(:controller => 'citations', :action => 'index')
+    expire_fragment(:controller => 'citations', :action => 'index', :action_suffix => "citation_#{citation.id}")
     expire_fragment(:controller => 'citations', :action=> 'show', :id=>citation.id)
   end
 end
