99fd3b10f795f7ee9236db2bffc9005ee79d5bd3
Sven Bohm <bohms@msu.edu>
2012-02-14 15:53:18 -0500

    removed extra mocha require and fixed deprecation warning from factory girl

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,7 +6,7 @@ require File.expand_path(File.dirname(__FILE__) + "../../config/environment")
 require 'rails/test_help'
 require 'shoulda'
 require 'factory_girl'
-Factory.find_definitions
+FactoryGirl.find_definitions
 require Rails.root.join('test', 'shoulda_macros', 'paperclip')
 
 require "#{Rails.root}/db/seeds.rb"
