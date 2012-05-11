47942af070650ae86c4f2d9ad8b73628e7cd2c99
Sven Bohm <bohms@msu.edu>
2012-05-11 15:20:59 -0400

    send session info over https

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,9 +3,10 @@
 if Rails.env.development?
   Metadata::Application.config.session_store :cookie_store, :key => '_metadata_session'
 else
-
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rake db:sessions:create")
-  Metadata::Application.config.session_store :active_record_store #, :domain => :all
+  Metadata::Application.config.session_store :active_record_store, {
+    :secure => true
+  }
 end
