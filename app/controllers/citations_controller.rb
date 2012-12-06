95edfe060aa05fec0d43f29d5bf5a3d176924b42
Sven Bohm <bohms@msu.edu>
2012-12-06 14:41:29 -0500

    fixed changes due to paperclip using aws-sdk now

--- app/controllers/citations_controller.rb
+++ app/controllers/citations_controller.rb
@@ -126,7 +126,7 @@ class CitationsController < ApplicationController
 
     path = citation.pdf.path(params[:style])
     if Rails.env.production?
-      redirect_to(AWS::S3::S3Object.url_for(path, citation.pdf.bucket_name, :expires_in => 10.seconds))
+      redirect_to(citation.pdf.s3_object(params[:style]).url_for(:read ,:secure => true, :expires_in => 10.seconds).to_s)
     else
       send_file  path, :type => 'application/pdf', :disposition => 'inline'
     end
