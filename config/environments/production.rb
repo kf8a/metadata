cca04909dc6e77769bc0ec3ccd963cdc65b483e1
Sven Bohm <bohms@msu.edu>
2012-12-12 10:26:37 -0500

    set up for use with wildcard certs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,6 +41,10 @@ Metadata::Application.configure do
   # Enable serving of images, stylesheets, and javascripts from an asset server
   #config.action_controller.asset_host = "http://lter.kbs.msu.edu/dynamic"
   
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,  
+    :openssl_verify_mode  => 'none' 
+  }
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
