276408a9cb76708974193f2969fe610325b490c4
Sven Bohm <bohms@msu.edu>
2012-12-19 11:00:45 -0500

    only show the ones that have an abstract

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -38,7 +38,7 @@ class PublicationsController < ApplicationController
   end
 
   def remaining
-    @publications = Publication.where('publication_type_id <> 6').where(:deprecated => false)
+    @publications = Publication.where('publication_type_id <> 6').where(:deprecated => false).where('abstract is not null')
   end
 
   def index_by_treatment
