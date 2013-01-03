c4c3f9cabce4a1f92db2c08ddf0c27aa2febbf58
Sven Bohm <bohms@msu.edu>
2013-01-03 16:21:10 -0500

    removed poonam from the email list

--- app/helpers/datatables_helper.rb
+++ app/helpers/datatables_helper.rb
@@ -3,7 +3,7 @@ module DatatablesHelper
   def permission_request_email_list(datatable)
     emails = datatable.owners.collect {|owner| owner.email}
     emails << 'glbrc.data@kbs.msu.edu'
-    emails << 'poonam31@msu.edu'
+    emails << 'sippel@msu.edu'
     emails.join(',')
   end
 
