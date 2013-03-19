4be627a270d89440f9bef8ca979e98ed36c8fcfb
Sven Bohm <bohms@msu.edu>
2013-03-19 17:30:10 -0400

    removed call to remarkable

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,6 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'factory_girl'
 FactoryGirl.reload
-require 'remarkable/active_record'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
