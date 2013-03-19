a8d7cebc6290972494d12e289f2f7bf03aec4764
Sven Bohm <bohms@msu.edu>
2013-03-19 17:34:41 -0400

    removed old text

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,14 +13,6 @@ require Rails.root.join('test', 'shoulda_macros', 'paperclip')
 require "#{Rails.root}/db/seeds.rb"
 
 class ActiveSupport::TestCase
-  # Transactional fixtures accelerate your tests by wrapping each test method
-  # in a transaction that's rolled back on completion.  This ensures that the
-  # test database remains unchanged so your fixtures don't have to be reloaded
-  # between every test method.  Fewer database queries means faster tests.
-  #
-  # Read Mike Clark's excellent walkthrough at
-  #   http://clarkware.com/cgi/blosxom/2005/10/24#Rails10FastTesting
-  #
   # Every Active Record database supports transactions except MyISAM tables
   # in MySQL.  Turn off transactional fixtures in this case; however, if you
   # don't care one way or the other, switching from MyISAM to InnoDB tables
