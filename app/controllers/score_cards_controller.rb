79dae129b087edef797adf089626bf721a3761f4
Sven Bohm <bohms@msu.edu>
2013-03-21 13:02:42 -0400

    compute all of them

--- app/controllers/score_cards_controller.rb
+++ app/controllers/score_cards_controller.rb
@@ -1,5 +1,5 @@
 class ScoreCardsController < ApplicationController
   def index
-    @datatables = Datatable.includes(:dataset).where(:is_sql => true).where('datasets.website_id = 1').limit(5).all
+    @datatables = Datatable.includes(:dataset).where(:is_sql => true).where('datasets.website_id = 1').all
   end
 end
