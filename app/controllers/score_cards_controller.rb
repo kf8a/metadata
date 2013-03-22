86744b28fe2864297b1fcb02167e7584a833506f
Sven Bohm <bohms@msu.edu>
2013-03-22 16:08:53 -0400

    only show active datasets by default

--- app/controllers/score_cards_controller.rb
+++ app/controllers/score_cards_controller.rb
@@ -3,10 +3,13 @@ class ScoreCardsController < ApplicationController
     # @datatables = Study.includes(:datatables).where('datatables.is_sql is true').where(:code => 'MSCE')
 
     on_web = params[:on_web] || true
+    status = params[:status] || 'active'
     if 'glbrc' == website
       @datatables = Datatable.includes(:dataset).where(:is_sql => true).where('datasets.website_id = 2')
     else
-      @datatables = Datatable.includes(:dataset).where(:is_sql => true).where('datasets.website_id = 1').where(:on_web => on_web).order(:study_id).order(:theme_id)
+      @datatables = Datatable.includes(:dataset).where(:is_sql => true).where('datasets.website_id = 1')
+                              .where(:on_web => on_web).where('datasets.status = ?', status)
+                              .order(:study_id).order(:theme_id)
     end
   end
 end
