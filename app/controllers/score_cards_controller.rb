876e84aaa59a519e38753c5637d28debb3971f91
Sven Bohm <bohms@msu.edu>
2013-03-22 08:43:11 -0400

    styles and layout tweaks on scorecard

--- app/controllers/score_cards_controller.rb
+++ app/controllers/score_cards_controller.rb
@@ -1,6 +1,8 @@
 class ScoreCardsController < ApplicationController
   def index
     limit = params[:limit] || 25
+    # @datatables = Study.includes(:datatables).where('datatables.is_sql is true').where(:code => 'MSCE')
+
     @datatables = Datatable.includes(:dataset).where(:is_sql => true).where('datasets.website_id = 1').limit(limit.to_i).all
   end
 end
