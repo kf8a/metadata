49632d3f0532aed4fd7b5821d9e1e5e3c1b1eeaf
Sven Bohm <bohms@msu.edu>
2013-03-22 13:05:58 -0400

    only show on_web datatables

--- app/controllers/score_cards_controller.rb
+++ app/controllers/score_cards_controller.rb
@@ -2,10 +2,11 @@ class ScoreCardsController < ApplicationController
   def index
     # @datatables = Study.includes(:datatables).where('datatables.is_sql is true').where(:code => 'MSCE')
 
+    on_web = params[:on_web] || true
     if 'glbrc' == website
       @datatables = Datatable.includes(:dataset).where(:is_sql => true).where('datasets.website_id = 2')
     else
-      @datatables = Datatable.includes(:dataset).where(:is_sql => true).where('datasets.website_id = 1').order(:study_id)
+      @datatables = Datatable.includes(:dataset).where(:is_sql => true).where('datasets.website_id = 1').where(:on_web => on_web).order(:study_id).order(:theme_id)
     end
   end
 end
