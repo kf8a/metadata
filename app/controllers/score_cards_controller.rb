866390b6e69abc31b19a6015ce9bc4f624667284
Sven Bohm <bohms@msu.edu>
2013-03-26 09:43:45 -0400

    sort the glbrc website too

--- app/controllers/score_cards_controller.rb
+++ app/controllers/score_cards_controller.rb
@@ -7,8 +7,9 @@ class ScoreCardsController < ApplicationController
     website  = params[:website] || 'lter'
     study_id = params[:study_id]
 
-    if 'glbrc' == website
+    if 'glbrc' == website.downcase
       @datatables = Datatable.includes(:dataset).where(:is_sql => true).where('datasets.website_id = 2')
+                             .where(:on_web => on_web).order(:study_id).order(:theme_id).order('datatables.id')
     else
       @datatables = Datatable.includes(:dataset).where(:is_sql => true).where('datasets.website_id = 1')
                               .where(:on_web => on_web).where('datasets.status = ?', status)
