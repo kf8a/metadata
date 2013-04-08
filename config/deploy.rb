0a6ef097b32d7c74bc6a8862bdd276f09e89b197
Sven Bohm <bohms@msu.edu>
2013-04-07 22:10:01 -0400

    added copy to asset host

--- config/deploy.rb
+++ config/deploy.rb
@@ -79,6 +79,7 @@ namespace :deploy do
     task :precompile, roles: :web, except: {no_release: true} do
       run_locally "bundle exec rake assets:precompile"
       find_servers_for_task(current_task).each do |server|
+        run_locally "rsync -vr --exclude='.DS_Store' public/assets #{user}@#{server.host}:#{shared_path}/"
         run_locally "rsync -vr --exclude='.DS_Store' public/assets #{asset_host}:#{asset_path}/"
       end
     end
