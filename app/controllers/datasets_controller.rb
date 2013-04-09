8fa09f4960bffbc63b2233f19082a32f47451c54
Sven Bohm <bohms@msu.edu>
2013-04-09 16:54:30 -0400

    only show the current websites datasets

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -19,7 +19,7 @@ class DatasetsController < ApplicationController
     @keyword_list   = params['keyword_list']
     @people         = Person.find_all_with_dataset
     @themes         = Theme.by_weight
-    @datasets       = Dataset.where(:on_web => true).all
+    @datasets       = Dataset.where(:on_web => true).where(:website_id => website.id)
     @studies        = collect_and_normalize_studies(@datasets)
     @crumbs         = []
     respond_to do |format|
