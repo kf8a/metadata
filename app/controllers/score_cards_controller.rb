516c5c0964802b72e942dc71140654c443a06ac7
Sven Bohm <bohms@msu.edu>
2013-04-15 08:10:08 -0400

    show all datatables on the front page

--- app/controllers/score_cards_controller.rb
+++ app/controllers/score_cards_controller.rb
@@ -3,7 +3,6 @@ class ScoreCardsController < ApplicationController
     # @datatables = Study.includes(:datatables).where('datatables.is_sql is true').where(:code => 'MSCE')
 
     on_web   = params[:on_web] || true
-    status   = params[:status] || 'active'
     website  = params[:website] || 'lter'
     study_id = params[:study_id]
 
@@ -12,8 +11,7 @@ class ScoreCardsController < ApplicationController
                              .where(:on_web => on_web).order(:study_id).order(:theme_id).order('datatables.id')
     else
       @datatables = Datatable.includes(:dataset).where(:is_sql => true).where('datasets.website_id = 1')
-                              .where(:on_web => on_web).where('datasets.status = ?', status)
-                              .order(:study_id).order(:theme_id).order('datatables.id')
+                              .where(:on_web => on_web).order(:study_id).order(:theme_id).order('datatables.id')
       if study_id
         @datatables = @datatables.where(:study_id => study_id)
       end
