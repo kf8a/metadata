f9fcb74608a0cf562a3075070858fb113a9e66ac
Sven Bohm <bohms@msu.edu>
2013-05-03 09:26:19 -0400

    use new s3 syntax

--- app/controllers/abstracts_controller.rb
+++ app/controllers/abstracts_controller.rb
@@ -15,7 +15,7 @@ class AbstractsController < ApplicationController
     head(:not_found) and return unless (abstract= Abstract.find_by_id(params[:id]))
     path = abstract.pdf.path(params[:style])
     if Rails.env.production?
-      redirect_to(AWS::S3::S3Object.url_for(path, abstract.pdf.bucket_name, :expires_in => 10.seconds))
+      redirect_to(abstract.pdf.s3_object(params[:style]).url_for(:read, :secure => true, :expires_in  => 20.seconds).to_s)
     else
       send_file  path, :type => 'application/pdf', :disposition => 'inline'
     end
