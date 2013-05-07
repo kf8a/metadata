30e50e36b1eb5da7fbe7a64de6c68e057d0adb35
Sven Bohm <bohms@msu.edu>
2013-05-07 15:02:03 -0400

    fixed issue with affiliations in person controller

--- app/assets/javascripts/people.js.coffee
+++ app/assets/javascripts/people.js.coffee
@@ -1,11 +1,10 @@
-jQuery ->
-  jQuery('form').on 'click', '.remove_fields', (event) ->
-    jQuery(this).prev('input[type=hidden]').val('1')
-    jQuery(this).closest('fieldset').hide()
-    event.preventDefault()
+$(document).on 'click', 'form .remove_fields', (event) ->
+  $(this).prev('input[type=hidden]').val('1')
+  $(this).closest('fieldset').hide()
+  event.preventDefault()
 
-    jQuery('form').on 'click', '.add_fields', (event) ->
-      time = new Date().getTime()
-      regexp = new RegExp(jQuery(this).data('id'), 'g')
-      jQuery(this).before(jQuery(this).data('fields').replace(regexp, time))
-      event.preventDefault()
+$(document).on 'click', 'form .add_fields', (event) ->
+  time = new Date().getTime()
+  regexp = new RegExp($(this).data('id'), 'g')
+  $(this).before($(this).data('fields').replace(regexp, time))
+  event.preventDefault()
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -38,12 +38,13 @@ module ApplicationHelper
       form.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)")
   end
 
-  def link_to_add_fields(name, form, association)
-      new_object = form.object.class.reflect_on_association(association).klass.new
-        fields = form.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
-              render(association.to_s.singularize + "_fields", :f => builder)
-                end
-          link_to_function(name, "add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")")
+  def link_to_add_fields(name, f, association)
+    new_object = f.object.send(association).klass.new
+    id = new_object.object_id
+    fields = f.fields_for(association, new_object, child_index: id) do |builder|
+      render(association.to_s.singularize + "_fields", f: builder)
+    end
+    link_to(name, '#', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
   end
 
 end
