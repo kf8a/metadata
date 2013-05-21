7de0606ee7598afea9d3c27c7618a77dcc834af7
Sven Bohm <bohms@msu.edu>
2013-05-21 09:54:20 -0400

    use s3 for dataset files

--- config/deploy.rb
+++ config/deploy.rb
@@ -76,7 +76,7 @@ namespace :deploy do
 
   namespace :assets do
     desc "Precompile assets on local machine and upload them to the server."
-    task :precompile, roles: :web, except: {no_release: true} do
+    task :precompile, :roles => :web, except: {no_release: true} do
       run_locally "bundle exec rake assets:clean"
       run_locally "bundle exec rake assets:precompile"
       find_servers_for_task(current_task).each do |server|
@@ -117,7 +117,7 @@ task :kalkaska do
   set :asset_path, '/var/www/lter/metadata-assets'
   role :web, "#{host}.kbs.msu.edu"
   role :app, "#{host}.kbs.msu.edu"
-  role :db, "#{host}.kbs.msu.edu"
+  role :db, "kalkaska.kbs.msu.edu", :primary => true
 end
 
 task :houghton do
