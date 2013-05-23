0989728319f0d231dca85836873bd3af014a3114
Sven Bohm <bohms@msu.edu>
2013-05-23 08:26:03 -0400

    use the right attribute in dataset_files_controller

--- app/controllers/dataset_files_controller.rb
+++ app/controllers/dataset_files_controller.rb
@@ -1,9 +1,9 @@
 class DatasetFilesController < ApplicationController
   def show 
     dataset_file = DatasetFile.find(params[:id])
-    path = dataset_file.pdf.path(params[:style])
+    path = dataset_file.data.path(params[:style])
     if Rails.env.production?
-      redirect_to(dataset_file.pdf.s3_object(params[:style]).url_for(:read ,:secure => true, :expires_in => 10.seconds).to_s)
+      redirect_to(dataset_file.data.s3_object(params[:style]).url_for(:read ,:secure => true, :expires_in => 10.seconds).to_s)
     else
       send_file  path, :type => 'application/pdf', :disposition => 'inline'
     end
