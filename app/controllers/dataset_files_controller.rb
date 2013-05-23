0e453b22cae7fabb751ddb7d6595cdebba1c06f3
Sven Bohm <bohms@msu.edu>
2013-05-23 08:21:49 -0400

    oops

--- app/controllers/dataset_files_controller.rb
+++ app/controllers/dataset_files_controller.rb
@@ -3,7 +3,7 @@ class DatasetFilesController < ApplicationController
     dataset_file = DatasetFile.find(params[:id])
     path = dataset_file.pdf.path(params[:style])
     if Rails.env.production?
-      redirect_to(citation.pdf.s3_object(params[:style]).url_for(:read ,:secure => true, :expires_in => 10.seconds).to_s)
+      redirect_to(dataset_file.pdf.s3_object(params[:style]).url_for(:read ,:secure => true, :expires_in => 10.seconds).to_s)
     else
       send_file  path, :type => 'application/pdf', :disposition => 'inline'
     end
