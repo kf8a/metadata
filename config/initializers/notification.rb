10635ff36b3a7b1a521cd7754b32f1aa0166360c
Sven Bohm <bohms@msu.edu>
2013-11-08 09:31:12 -0500

    use ms

--- config/initializers/notification.rb
+++ config/initializers/notification.rb
@@ -2,6 +2,6 @@ require 'cube'
 
 ActiveSupport::Notifications.subscribe "process_action.action_controller" do |name, start, finish, id, payload|
   cube = Cube::Client.new 'gprpc32.kbs.msu.edu'
-  duration = finish - start
+  duration = (finish - start) * 1000
   cube.send 'md', start, response: duration, path: payload[:path]
 end
