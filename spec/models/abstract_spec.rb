3dc48fcadeca9dba57c0c80fd330bd468094865c
Sven Bohm <bohms@msu.edu>
2015-07-06 10:22:10 -0400

    we no longer require an abstract for the meeting presentations

--- spec/models/abstract_spec.rb
+++ spec/models/abstract_spec.rb
@@ -2,5 +2,4 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Abstract do
   it {should validate_presence_of :meeting}
-  it {should validate_presence_of :abstract}
 end
