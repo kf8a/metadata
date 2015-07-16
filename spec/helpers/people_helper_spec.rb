f7eae3254b2dc80c89e3ab5a76df1719a6763c09
Sven Bohm <bohms@msu.edu>
2015-07-16 10:05:31 -0700

    updated rspec requirement

--- spec/helpers/people_helper_spec.rb
+++ spec/helpers/people_helper_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
-describe PeopleHelper do
+describe PeopleHelper, type: :helper do
   describe "#contact_link" do
     it "should change @ to at" do
       unmodified_email = "sam@example.com"
       fixed_email = "sam at example.com"
-      helper.contact_link(unmodified_email).should eq(fixed_email)
+      expect(helper.contact_link(unmodified_email)).to eq(fixed_email)
     end
   end
 end
