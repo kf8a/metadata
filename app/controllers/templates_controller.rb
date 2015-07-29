80b677a351a5f1395eb90c0d50149b3cf7b4c368
Sven Bohm <bohms@msu.edu>
2015-07-29 14:55:57 -0400

    updated templates controller and removed formtastic

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -1,27 +1,33 @@
+#TODO do we need to keep this
 class TemplatesController < ApplicationController
 
   before_filter :admin?, :except => [:index, :show]  if Rails.env == 'production'
   respond_to :html, :xml, :json
-  
+
   def index
-    @templates = Template.find(:all)
+    @templates = Template.all
   end
-  
+
   def show
     @template = Template.find(params[:id])
   end
-  
+
   def new
     @template = Template.new
   end
-  
+
   def create
-    respond_with(@template = Template.create(params[:template]))
+    respond_with(@template = Template.create(template_params))
   end
-  
+
   def update
   end
-  
+
   def delete
+  end
+
+  private
+  def template_params
+    params.require(:template).permit(:website_id, :controller, :action, :layout)
   end
 end
