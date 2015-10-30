151f1b8cd8bae2fb34d990bc0e8fc53b7e9ffe02
Sven Bohm <bohms@msu.edu>
2015-10-30 14:53:54 -0400

    moved page title to the actions

--- spec/models/citations/chapter_citation_spec.rb
+++ spec/models/citations/chapter_citation_spec.rb
@@ -9,34 +9,35 @@ describe ChapterCitation do
   describe 'formatting' do
     before do 
       @chapter = ChapterCitation.new
-      @chapter.stub(:author_and_year) { 'Jones 1981.' }
-      @chapter.stub(:title) { 'Chapter 10' }
-      @chapter.stub(:publication) { 'Lifetime Books' }
+      allow(@chapter).to receive(:author_and_year).and_return 'Jones 1981.'
+
+      allow(@chapter).to receive(:title).and_return 'Chapter 10'
+      allow(@chapter).to receive(:publication).and_return 'Lifetime Books'
     end
 
     it 'should be formatted correctly with volume and pages' do
-      @chapter.stub(:volume) { '2' }
-      @chapter.stub(:start_page_number) {'1'}
-      @chapter.stub(:ending_page_number) {'10'}
-      @chapter.stub(:address_and_city) {"New York. New York"}
-      @chapter.formatted.should == "Jones 1981. Chapter 10. Vol 2, Pages 1-10 in Lifetime Books. New York. New York."
+      allow(@chapter).to receive(:volume).and_return '2'
+      allow(@chapter).to receive(:start_page_number).and_return '1'
+      allow(@chapter).to receive(:ending_page_number).and_return '10'
+      allow(@chapter).to receive(:address_and_city).and_return "New York. New York"
+      expect(@chapter.formatted).to eq "Jones 1981. Chapter 10. Vol 2, Pages 1-10 in Lifetime Books. New York. New York."
     end
 
     it 'should be formatted correctly without volume' do
-      @chapter.stub(:start_page_number) {'1'}
-      @chapter.stub(:ending_page_number) {'10'}
-      @chapter.stub(:address_and_city) {"New York. New York"}
-      @chapter.formatted.should == "Jones 1981. Chapter 10. Pages 1-10 in Lifetime Books. New York. New York."
+      allow(@chapter).to receive(:start_page_number).and_return '1'
+      allow(@chapter).to receive(:ending_page_number).and_return '10'
+      allow(@chapter).to receive(:address_and_city).and_return "New York. New York"
+      expect(@chapter.formatted).to eq "Jones 1981. Chapter 10. Pages 1-10 in Lifetime Books. New York. New York."
     end
   end
 
-  it 'should be formatted correctly with editors' do
-    pending "write a better test"
-    chapter = ChapterCitation.new
-    chapter.stub(:author_and_year) {"Pryor, S. C., D. Scavia, C. Downer, M. Gaden, L. Iverson, R. Nordstrom, J. Patz, and G. P. Robertson. 2014."}
-    chapter.stub(:publication) {"Climate change impacts in the United States: The Third National Climate Assessment"}
-    chapter.stub(:doi) {"10.7930/J0J1012N"}
-    chapter.stub(:eds) {"J. M. Melillo, T. C. Richmond, and G. W. Yohe eds."} 
-    chapter.formatted.should == "Pryor, S. C., D. Scavia, C. Downer, M. Gaden, L. Iverson, R. Nordstrom, J. Patz, and G. P. Robertson. 2014. Chapter 18: Midwest. Pages 418-440 in J. M. Melillo, T. C. Richmond, and G. W. Yohe, eds. Climate change impacts in the United States: The Third National Climate Assessment. U.S. Global Change Research Program, doi:10.7930/J0J1012N"
-  end
+  # it 'should be formatted correctly with editors' do
+  #   pending "write a better test"
+  #   # chapter = ChapterCitation.new
+  #   # chapter.stub(:author_and_year) {"Pryor, S. C., D. Scavia, C. Downer, M. Gaden, L. Iverson, R. Nordstrom, J. Patz, and G. P. Robertson. 2014."}
+  #   # chapter.stub(:publication) {"Climate change impacts in the United States: The Third National Climate Assessment"}
+  #   # chapter.stub(:doi) {"10.7930/J0J1012N"}
+  #   # chapter.stub(:eds) {"J. M. Melillo, T. C. Richmond, and G. W. Yohe eds."} 
+  #   # chapter.formatted.should == "Pryor, S. C., D. Scavia, C. Downer, M. Gaden, L. Iverson, R. Nordstrom, J. Patz, and G. P. Robertson. 2014. Chapter 18: Midwest. Pages 418-440 in J. M. Melillo, T. C. Richmond, and G. W. Yohe, eds. Climate change impacts in the United States: The Third National Climate Assessment. U.S. Global Change Research Program, doi:10.7930/J0J1012N"
+  # end
 end
