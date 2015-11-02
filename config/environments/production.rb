6fbc38fdcf4a84fe963b32d4bdc5b895224c90f3
Sven Bohm <bohms@msu.edu>
2015-11-02 10:33:43 -0500

    use the right directory

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = 'http://lter.kbs.msu.edu/metadata-assets/'
+  config.action_controller.asset_host = 'http://lter.kbs.msu.edu/metadata-assets/assets/'
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
