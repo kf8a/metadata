5919553ddab46eeabd5935eb5025434f14179861
Sven Bohm <bohms@msu.edu>
2015-11-09 15:15:48 -0500

    one more includes without references

--- app/controllers/citations_controller.rb
+++ app/controllers/citations_controller.rb
@@ -65,7 +65,7 @@ class CitationsController < ApplicationController
 
   def index_by_author
       citations = [website.citations.publications]
-      @citations = citations.collect {|c| c.includes(:authors).published}.flatten.sort {|a,b| a.authors.first.try(:sur_name) <=> b.authors.first.try(:sur_name) }
+      @citations = citations.collect {|c| c.includes(:authors).references(:authors).published}.flatten.sort {|a,b| a.authors.first.try(:sur_name) <=> b.authors.first.try(:sur_name) }
       index_responder
   end
 
