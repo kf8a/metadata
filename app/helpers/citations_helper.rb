d7ccdc6eddfb055a966905edda0bd0660731ad3f
Sven Bohm <bohms@msu.edu>
2015-11-19 13:34:30 -0500

    eliminate the dots from the download url not from the S3 url

--- app/helpers/citations_helper.rb
+++ app/helpers/citations_helper.rb
@@ -1,7 +1,7 @@
 module CitationsHelper
   def publication_url(citation) 
     if citation.open_access
-      download_citation_url(citation) + "/#{citation.pdf_file_name}"
+      download_citation_url(citation) + "/#{citation.pdf_file_name.gsub(/\.(?!\w{3}$)/, '+')}"
     else
       download_citation_url(citation)
     end
