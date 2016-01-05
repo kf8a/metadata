e5bce48e1c4ce82cba0601a0a188a0aa3ccad1ae
Sven Bohm <bohms@msu.edu>
2016-01-05 10:48:55 -0500

    updated old rspec syntax

--- spec/models/citations/bulletin_citation_spec.rb
+++ spec/models/citations/bulletin_citation_spec.rb
@@ -8,11 +8,11 @@ describe BulletinCitation do
 
   it 'should be formatted correctly' do
     report = BulletinCitation.new
-    report.stub(:author_and_year) {'Snapp, S. S., and V. L. Morrone. 2014.'}
-    report.stub(:title) {'Perennial wheat'}
-    report.stub(:publisher) {"Michigan State University"}
-    report.stub(:city) {'East Lansing, Michigan, USA'}
-    report.stub(:publication) {'MSU Extension Bulletin E-3208'}
+    allow(report).to receive(:author_and_year).and_return 'Snapp, S. S., and V. L. Morrone. 2014.'
+    allow(report).to receive(:title).and_return 'Perennial wheat'
+    allow(report).to receive(:publisher).and_return "Michigan State University"
+    allow(report).to receive(:city).and_return 'East Lansing, Michigan, USA'
+    allow(report).to receive(:publication).and_return 'MSU Extension Bulletin E-3208'
     expect(report.formatted).to eq "Snapp, S. S., and V. L. Morrone. 2014. Perennial wheat. MSU Extension Bulletin E-3208, Michigan State University, East Lansing, Michigan, USA."
   end
 end
