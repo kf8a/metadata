806c9443987346925611072f0bd03e438a730e85
Sven Bohm <bohms@msu.edu>
2016-02-19 19:27:01 -0500

    increment version when datatable is touched

--- app/models/dataset.rb
+++ app/models/dataset.rb
@@ -25,6 +25,10 @@ class Dataset < ActiveRecord::Base
 
   acts_as_taggable_on :keywords
 
+  after_touch do
+    increment_version
+  end
+
   def self.from_eml_file(file)
     from_eml(file.read)
   end
