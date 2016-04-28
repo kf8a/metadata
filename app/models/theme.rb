f829dbf2636c5adc347ebab54fccfda8c5f2f0b0
Sven Bohm <bohms@msu.edu>
2016-04-27 21:05:07 -0400

    style fixes

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -4,8 +4,8 @@ class Theme < ActiveRecord::Base
   has_and_belongs_to_many :datasets
   has_many :datatables
 
-  scope :by_weight, -> {order :weight}
-  scope :by_name, -> { order 'name'}
+  scope :by_weight, -> { order :weight }
+  scope :by_name, -> { order 'name' }
 
   after_touch :update_parent
 
@@ -13,52 +13,49 @@ class Theme < ActiveRecord::Base
     '-' * level + name
   end
 
-  def datatables?(study=nil)
+  def datatables?(study = nil)
     i_have_datatables?(study) || children_have_datatables?(study)
   end
 
-  def i_have_datatables?(study=nil)
+  def i_have_datatables?(study = nil)
     study ? datatables_in_study(study).any? : datatables.any?
   end
 
-  def children_have_datatables?(study=nil)
-    children.collect {|subtheme| subtheme.datatables?(study)}.include?(true)
+  def children_have_datatables?(study = nil)
+    children.collect { |subtheme| subtheme.datatables?(study) }.include?(true)
   end
 
-  def include_datatables?(test_datatables=[])
+  def include_datatables?(test_datatables = [])
     my_datatables = self_and_descendants_datatables
     (my_datatables & test_datatables).any?
   end
 
   def include_datatables_from_study?(test_datatables, study)
     my_datatables = self_and_descendants_datatables
-    datatables_in_study = my_datatables.collect {|table| table if table.study == study }
+    datatables_in_study = my_datatables.collect { |table| table if table.study == study }
 
     (test_datatables & datatables_in_study).any?
   end
 
   def self_and_descendants_datatables
-    my_datatables = descendants.collect {|descendant| descendant.datatables }.flatten
+    my_datatables = descendants.collect { |descendant| descendant.datatables }.flatten
     my_datatables + datatables
   end
 
-  def datatables_in_study(study, test_datatables=[])
+  def datatables_in_study(study, test_datatables = [])
     if test_datatables.any?
-      datatables.collect {|table| table if table.study == study and test_datatables.include?(table)}.compact
+      datatables.collect { |table| table if table.study == study && test_datatables.include?(table) }.compact
     else
-      datatables.collect {|table| table if table.study == study }.compact
+      datatables.collect { |table| table if table.study == study }.compact
     end
   end
 
   private
 
   def update_parent
-    self.parent.try(:touch)
+    parent.try(:touch)
   end
 end
-
-
-
 
 # == Schema Information
 #
@@ -71,4 +68,3 @@ end
 #  lft       :integer
 #  rgt       :integer
 #
-
--- app/models/treatment.rb
+++ app/models/treatment.rb
@@ -1,10 +1,8 @@
+# Represents a treatment that is part of a study
 class Treatment < ActiveRecord::Base
   belongs_to :study
   has_and_belongs_to_many :citations
 end
-
-
-
 
 # == Schema Information
 #
