55ad180e545510991571ecf30d97d8c406873b65
Sven Bohm <bohms@msu.edu>
2016-04-28 11:22:03 -0400

    style cleanup

--- app/models/score_card.rb
+++ app/models/score_card.rb
@@ -13,14 +13,14 @@ class ScoreCard
 
   def db_connection
     unless @db_connection
-     @db_connection = ActiveRecord::Base.connection
+      @db_connection = ActiveRecord::Base.connection
     end
     @db_connection
   end
 
   def score(datatable)
     # exclude climdb datatables and archive
-    return if [309, 300, 301,175, 127,82].include? datatable.id
+    return if [309, 300, 301, 175, 127, 82].include? datatable.id
     result = {}
     time_key = datatable.variate_names.grep(/year/i).first
     unless time_key
@@ -28,7 +28,7 @@ class ScoreCard
     end
 
     if time_key
-      result = data(datatable,time_key)
+      result = data(datatable, time_key)
 
       result = fill_to_present(result, update_frequency_years(datatable)) unless 'completed' == datatable.status
       result
@@ -39,7 +39,7 @@ class ScoreCard
 
   def update_frequency_years(datatable)
     update_frequency = datatable.update_frequency_days || 365
-    update_frequency = update_frequency / 365
+    update_frequency /= 365
     update_frequency = 1 if 0 == update_frequency
     update_frequency
   end
@@ -61,11 +61,11 @@ class ScoreCard
       approved_result = []
     end
     result = query_result.collect do |row|
-      {:year => row['year'].to_i, :count => row['count'].to_f}
+      { year: row['year'].to_i, count: row['count'].to_f }
     end
 
     approved_result.each do |row|
-      r = result.index {|x| x[:year] == row['year'].to_i}
+      r = result.index { |x| x[:year] == row['year'].to_i }
       result[r][:approved] = row['count'].to_f
     end
 
@@ -74,21 +74,21 @@ class ScoreCard
 
   def fill_to_present(data, update_frequency_years = 1)
     return if data.empty?
-    max_year_record = data.max {|a,b| a[:year] <=> b[:year]}
+    max_year_record = data.max { |a, b| a[:year] <=> b[:year] }
     max_year = max_year_record[:year].to_i
     max_year += update_frequency_years
-    add_years = (max_year..current_year).step(update_frequency_years).collect { |year| {:year => year, :count => 0} }
+    add_years = (max_year..current_year).step(update_frequency_years).collect { |year| { year: year, count: 0 } }
     add_years + data
   end
 
   def current_year
-    Time.now().year
+    Time.zone.now.year
   end
 
   def self.update_all
     score = ScoreCard.new
-    Datatable.where(:is_sql => true).all.each do |datatable|
-      datatable.scores  = score.score(datatable).to_json
+    Datatable.where(is_sql: true).all.each do |datatable|
+      datatable.scores = score.score(datatable).to_json
       datatable.save
     end
   end
@@ -100,11 +100,16 @@ class ScoreCard
         next unless datatable.dataset.sponsor_id == 1
         next unless datatable.is_sql
         # exclude climdb datatables and archive
-        next if [309, 300, 301,175, 127,82].include? datatable.id
+        next if [309, 300, 301, 175, 127, 82].include? datatable.id
         s = score(datatable)
         if s
           s.each do |a|
-            csv << [datatable.id, datatable.completed, datatable.name, datatable.title, a[0],a[1][:score]]
+            csv << [datatable.id,
+                    datatable.completed,
+                    datatable.name,
+                    datatable.title,
+                    a[0],
+                    a[1][:score]]
           end
         end
       end
