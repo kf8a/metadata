ec240fb503eb91dd47085b3c208cc8a7caa85c45
Sven Bohm <bohms@msu.edu>
2016-05-22 14:04:49 -0400

    style fixes

--- app/controllers/datatables_controller.rb
+++ app/controllers/datatables_controller.rb
@@ -1,12 +1,12 @@
 class DatatablesController < ApplicationController
   helper_method :datatable
 
-  before_filter :admin?, :except => [:index, :show, :suggest, :search, :qc] unless Rails.env == 'development'
-  before_filter :can_download?, :only=>[:show], :if => Proc.new { |controller| controller.request.format.csv? || controller.request.format.fasta? } # run before filter to prevent non-members from downloading
+  before_action :admin?, except: [:index, :show, :suggest, :search, :qc] unless Rails.env == 'development'
+  before_action :can_download?, only: :show, if: Proc.new { |controller| controller.request.format.csv? || controller.request.format.fasta? } # run before filter to prevent non-members from downloading
   # before_filter :reject_robots
 
   helper_method :datatable
-  protect_from_forgery :except => [:index, :show, :search]
+  protect_from_forgery except: [:index, :show, :search]
 
   # GET /datatables
   # GET /datatables.xml
@@ -18,7 +18,7 @@ class DatatablesController < ApplicationController
 
     if Rails.env == 'production' #and stale? etag: @datatables
       respond_with @datatables do |format|
-        format.rss {render :rss => @datatables}
+        format.rss {render rss: @datatables}
       end
     end
   end
@@ -48,7 +48,7 @@ class DatatablesController < ApplicationController
         format.fasta
         format.csv do
           unless csv_ok
-            render :text => "You do not have permission to download this datatable"
+            render text: 'You do not have permission to download this datatable'
           end
 
           if current_user.try(:role) == 'admin'
@@ -74,7 +74,7 @@ class DatatablesController < ApplicationController
   # PUT publish
   def publish
     datatable.publish
-    render :nothing => true
+    render nothing: true
   end
 
 
@@ -114,7 +114,7 @@ class DatatablesController < ApplicationController
   # PUT /datatables/1.xml
   def update
     @core_areas = CoreArea.by_name.collect { |area| [area.name, area.id] }
-    @studies = Study.all.collect{ |study| [study.name, study.id]}
+    @studies = Study.all.collect{ |study| [study.name, study.id] }
     @people = Person.all
     @units = Unit.all
 
@@ -132,28 +132,28 @@ class DatatablesController < ApplicationController
     respond_with datatable
   end
 
-  #TODO only return the ones for the right website.
+  # TODO only return the ones for the right website
   def suggest
     term = params[:term]
 
-    list = ActsAsTaggableOn::Tag.where("lower(name) like ?", term.downcase + '%').select("DISTINCT tags.name")
+    list = ActsAsTaggableOn::Tag.where('lower(name) like ?', term.downcase + '%').select('DISTINCT tags.name')
     list = list + Person.where('lower(sur_name) like ?', term.downcase + '%').select('DISTINCT sur_name as name')
     list = list + Theme.where('lower(name) like ?', term.downcase + '%').select('DISTINCT name')
     list = list + CoreArea.where('lower(name) like ?', term.downcase + '%').select('DISTINCT name')
 
     keywords = list.collect {|x| x.name.downcase }.sort.uniq
     respond_to do |format|
-      format.json {render :json => keywords}
+      format.json { render json: keywords }
     end
   end
 
   def update_temporal_extent
     datatable.update_temporal_extent
     datatable.save
-    #TODO write test to make sure the js fragment get's rendered
+    # TODO write test to make sure the js fragment get's rendered
     respond_to do |format|
-      format.json 
-      format.html {render :nothing => true}
+      format.json
+      format.html { render nothing: true }
     end
   end
 
@@ -167,7 +167,7 @@ class DatatablesController < ApplicationController
 
   def set_title
     if @subdomain_request == 'lter'
-      @title  = 'LTER Data Catalog'
+      @title = 'LTER Data Catalog'
     else
       @title = 'GLBRC Sustainability Data Catalog'
     end
@@ -184,32 +184,31 @@ class DatatablesController < ApplicationController
 
     return unless params[:id]
 
-    #TODO this next line might not be needed
-    datatable  = Datatable.find(params[:id])
+    # TODO this next line might not be needed
+    datatable = Datatable.find(params[:id])
 
     if datatable.study
       study = datatable.study
-      #crumb.url = study_path(study)
+      # crumb.url = study_path(study)
       crumb.name = study.name
       @crumbs << crumb
     end
-
   end
 
   def retrieve_datatables(query)
     @default_value = 'Search for... '
     @themes = Theme.roots
 
-    @keyword_list = query['keyword_list'].sub(/\?|~|\\|\*|@|(?:=>)/, "")
+    @keyword_list = query['keyword_list'].sub(/\?|~|\\|\*|@|(?:=>)/, '')
     @keyword_list = nil if @keyword_list.empty? || @keyword_list == @default_value
 
     @datatables =
       if @keyword_list
-        Datatable.search @keyword_list, :with => {:website => website.id}
+        Datatable.search @keyword_list, with: { website: website.id }
       else
-        Datatable.where(:on_web => true).
-          includes(:dataset).references(:dataset).
-            where('is_secondary is false and website_id = ?', website.id)
+        Datatable.where(on_web: true)
+                 .includes(:dataset).references(:dataset)
+                 .where('is_secondary is false and website_id = ?', website.id)
       end
 
     @studies = Study.find_all_roots_with_datatables(@datatables)
@@ -231,13 +230,15 @@ class DatatablesController < ApplicationController
   end
 
   def datatable_params
-    params.require(:datatable).permit(:name, :title, :comments, :dataset_id, :data_url, :is_restricted, :is_secondary,
+    params.require(:datatable).permit(:name, :title, :comments, :dataset_id, :data_url,
+                                      :is_restricted, :is_secondary,
                                       :description, :begin_date, :end_date, :on_web, :keyword_list,
-                                     :theme_id, :weight, :study_id, :deprecation_notice, :update_frequency_days,
-                                     :is_secondary, {core_area_ids:[]}, 
-                                     {variates_attributes: [ [:name, :weight, :description, :unit_id, :measurement_scale, :data_type, :max_valid, 
-                                         :min_valid, :date_format, :precision, :missing_value_indicator, :_destroy, :id] ]} , 
-                                         {data_contributions_attributes: [[:person_id, :role_id, :_destroy, :id] ]})
+                                      :theme_id, :weight, :study_id, :deprecation_notice,
+                                      :update_frequency_days, :is_secondary, { core_area_ids: [] },
+                                      { variates_attributes: [[:name, :weight, :description, :unit_id,
+                                      :measurement_scale, :data_type, :max_valid,
+                                      :min_valid, :date_format, :precision, :missing_value_indicator, :_destroy, :id]] },
+                                      { data_contributions_attributes: [[:person_id, :role_id, :_destroy, :id]] })
   end
 
   private
@@ -260,19 +261,20 @@ class DatatablesController < ApplicationController
 
   def set_file_headers
     file_name = "#{@datatable.id}-#{@datatable.title.strip.gsub(/\W/,'+').squeeze('+')}.csv"
-    headers["Content-Type"] = "text/csv"
-    headers["Content-disposition"] = "attachment; filename=\"#{file_name}\""
+    headers['Content-Type'] = 'text/csv'
+    headers['Content-disposition'] = "attachment; filename=\"#{file_name}\""
   end
 
   def set_streaming_headers
-    #nginx doc: Setting this to "no" will allow unbuffered responses suitable for Comet and HTTP streaming applications
+    # nginx doc: Setting this to "no" will allow unbuffered responses
+    # suitable for Comet and HTTP streaming applications
     headers['X-Accel-Buffering'] = 'no'
 
-    headers["Cache-Control"] ||= "no-cache"
-    headers.delete("Content-Length")
+    headers['Cache-Control'] ||= 'no-cache'
+    headers.delete('Content-Length')
   end
 
-  def csv_data(query= @datatable.approved_data_query)
+  def csv_data(query = @datatable.approved_data_query)
     Enumerator.new do |line|
       line << @datatable.csv_headers.to_s
 
@@ -281,12 +283,10 @@ class DatatablesController < ApplicationController
       end
     end
   end
-  
 
   # def reject_robots
   #   if params[:id] == 'robots'
   #     render :status => 404
   #   end
   # end
-
 end
