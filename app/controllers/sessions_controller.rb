848e0889e676edc799197f9b529bb5c74e09604b
Sven Bohm <bohms@msu.edu>
2016-06-02 14:13:07 -0400

    removed debugging statements

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,15 @@
+# create new session for logging in
 class SessionsController < Clearance::SessionsController
   def new
   end
 
   def create
-    if params[:session]
-      session = params[:session]
-    end
     # set the default user if blank or 'lter'
     if params[:session]
       session = params[:session]
-      logger.info(session)
-      logger.info(session[:email])
       if session[:email].empty? || session[:email].casecmp('lter') == 0
         params[:session][:email] = 'lter@kbs.edu'
       end
-      logger.info(session)
-      logger.info(session[:email])
     end
     super # let clearance handle it
   end
