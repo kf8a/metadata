f0b8e9b759e5e70f62f805e828c6b31ab123e820
Sven Bohm <bohms@msu.edu>
2016-06-13 12:37:55 -0400

    style fixes

--- app/models/article_citation.rb
+++ app/models/article_citation.rb
@@ -1,7 +1,8 @@
 # A peer reviewed article
 class ArticleCitation < Citation
   def formatted(options = {})
-    "#{author_and_year(options)} #{title_and_punctuation} #{journal} #{volume_and_page} #{annotation}".rstrip
+    "#{author_and_year(options)} #{title_and_punctuation}"\
+    " #{journal} #{volume_and_page} #{annotation}".rstrip
   end
 
   private
@@ -18,42 +19,3 @@ class ArticleCitation < Citation
     "Journal Article\n"
   end
 end
-
-# == Schema Information
-#
-# Table name: citations
-#
-#  id                      :integer         not null, primary key
-#  title                   :text
-#  abstract                :text
-#  pub_date                :date
-#  pub_year                :integer
-#  citation_type_id        :integer
-#  address                 :text
-#  notes                   :text
-#  publication             :string(255)
-#  start_page_number       :integer
-#  ending_page_number      :integer
-#  periodical_full_name    :text
-#  periodical_abbreviation :string(255)
-#  volume                  :string(255)
-#  issue                   :string(255)
-#  city                    :string(255)
-#  publisher               :string(255)
-#  secondary_title         :string(255)
-#  series_title            :string(255)
-#  isbn                    :string(255)
-#  doi                     :string(255)
-#  full_text               :text
-#  publisher_url           :string(255)
-#  website_id              :integer
-#  created_at              :datetime
-#  updated_at              :datetime
-#  pdf_file_name           :string(255)
-#  pdf_content_type        :string(255)
-#  pdf_file_size           :integer
-#  pdf_updated_at          :datetime
-#  state                   :string(255)
-#  open_access             :boolean         default(FALSE)
-#  type                    :string(255)
-#
