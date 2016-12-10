3e5fa8cd73c51e0f4e27a8afe55a3e4b68a8ea4d
Sven Bohm <bohms@msu.edu>
2016-12-09 20:18:28 -0500

    style fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -39,7 +39,7 @@ module ApplicationHelper
     fields = f.fields_for(association, new_object, child_index: id) do |builder|
       render(association.to_s.singularize + '_fields', f: builder)
     end
-    link_to(name, '#', class: 'add_fields', data: {id: id, fields: fields.gsub("\n", '')})
+    link_to(name, '#', class: 'add_fields', data: { id: id, fields: fields.delete("\n") })
   end
 
   # TODO: do remove in favor of unobtrusive javascript
