fc2b524a111156dbc52094e85ce37cf6f551b3fe
Sven Bohm <bohms@msu.edu>
2016-12-16 07:52:03 -0500

    style fixes

--- lib/element_handler.rb
+++ lib/element_handler.rb
@@ -1,33 +1,33 @@
+# Handle elememnts when parsing eml documents
 class ElementHandler
-  
-  def apply element
-    element.each { |node| handle(node)} if element
+  def apply(element)
+    element.each { |node| handle(node) } if element
   end
 
-  def handle node
-    if node.kind_of? REXML::Text
-      handleTextNode(node)
-    elsif node.kind_of? REXML::Element
+  def handle(node)
+    if node.is_a? REXML::Text
+      handle_text_node(node)
+    elsif node.is_a? REXML::Element
       handle_element node
     else
-      return #ignore comments and processing instructions
+      return # ignore comments and processing instructions
     end
   end
-  
-  def handle_element element
-    handler_method = "handle_" + element.name.tr("-","_")
-    if self.respond_to? handler_method
-      self.send(handler_method, element)
+
+  def handle_element(element)
+    handler_method = 'handle_' + element.name.tr('-', '_')
+    if respond_to? handler_method
+      send(handler_method, element)
     else
       default_handler(element)
     end
   end
 
-  def default_handler element
+  def default_handler(element)
     handle element
   end
-  
-  def handleTextNode textNode
-    textNode.value
+
+  def handle_text_mode(text_mode)
+    text_mode.value
   end
 end
