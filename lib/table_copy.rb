d0902f871cba5a6e9d6448af67a2ed40740c9d32
Sven Bohm <bohms@msu.edu>
2016-12-21 11:54:20 -0500

    style fixes

--- lib/table_copy.rb
+++ lib/table_copy.rb
@@ -9,9 +9,9 @@ sth = old_table.prepare('select * from "DatasetAffiliations"')
 sth.execute
 while row = sth.fetch do
 
-  dataset_id = new_table.select_one("select id from datasets where dataset like ?", row[1])
-  person_id = new_table.select_one("select id from people where person like ?", row[0])
-  role_id = new_table.select_one("select id from roles where name like ?", row[2].downcase.strip.chop)
+  dataset_id = new_table.select_one('select id from datasets where dataset like ?', row[1])
+  person_id = new_table.select_one('select id from people where person like ?', row[0])
+  role_id = new_table.select_one('select id from roles where name like ?', row[2].downcase.strip.chop)
   puts row[0], person_id
   puts row[2], role_id
   puts row[1], dataset_id
@@ -21,9 +21,8 @@ while row = sth.fetch do
       values(?,?,?,?)", 
       person_id, role_id, dataset_id, row[3])
  end
- 
- t.column "person_id",  :integer
- t.column "role_id",    :integer
- t.column "dataset_id", :integer
- t.column "seniority",  :integer
- 
+
+ t.column 'person_id',  :integer
+ t.column 'role_id',    :integer
+ t.column 'dataset_id', :integer
+ t.column 'seniority',  :integer
