b00c8415abd76b9b2c30cefa1935af4ae5023829
Sven Bohm <bohms@msu.edu>
2017-10-30 11:02:44 -0400

    style fix

--- app/models/citation.rb
+++ app/models/citation.rb
@@ -29,7 +29,7 @@ class Citation < ActiveRecord::Base
                     storage: :s3,
                     bucket: 'metadata-production',
                     path: '/citations/pdfs/:id/:style/:basename.:extension',
-                    s3_credentials: File.join(Rails.root, 'config', 's3.yml'),
+                    s3_credentials: File.root.join(Rails.root, 'config', 's3.yml'),
                     s3_region: 'us-east-1',
                     s3_permissions: 'authenticated-read'
   #                 s3_headers: { 'Content-Disposition': 'attachment' }
@@ -495,42 +495,3 @@ class Citation < ActiveRecord::Base
     end
   end
 end
-
-# == Schema Information
-#
-# Table name: citations
-#
-#  id                      :integer         not null, primary key
-#  title                   :text
-#  abstract                :text
-#  pub_date                :date
-#  pub_year                :integer
-#  citation_type_id        :integer
-#  address                 :text
-#  notes                   :text
-#  publication             :string(255)
-#  start_page_number       :integer
-#  ending_page_number      :integer
-#  periodical_full_name    :text
-#  periodical_abbreviation :string(255)
-#  volume                  :string(255)
-#  issue                   :string(255)
-#  city                    :string(255)
-#  publisher               :string(255)
-#  secondary_title         :string(255)
-#  series_title            :string(255)
-#  isbn                    :string(255)
-#  doi                     :string(255)
-#  full_text               :text
-#  publisher_url           :string(255)
-#  website_id              :integer
-#  created_at              :datetime
-#  updated_at              :datetime
-#  pdf_file_name           :string(255)
-#  pdf_content_type        :string(255)
-#  pdf_file_size           :integer
-#  pdf_updated_at          :datetime
-#  state                   :string(255)
-#  open_access             :boolean         default(FALSE)
-#  type                    :string(255)
-#
