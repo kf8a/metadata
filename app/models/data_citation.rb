3c1b327c41e2c789668913bc2563df898f092960
Sven Bohm <bohms@msu.edu>
2017-10-31 12:20:50 -0400

    added a data citation class

---
+++ app/models/data_citation.rb
@@ -0,0 +1,21 @@
+# A data citation
+class DataCitation < Citation
+  def formatted(options = {})
+    "#{author_and_year(options)} #{title_and_punctuation}"\
+    " #{journal} #{volume_and_page} #{annotation}".rstrip
+  end
+
+  private
+
+  def journal
+    publication.blank? ? secondary_title : publication
+  end
+
+  def bibtex_type
+    :article
+  end
+
+  def endnote_type
+    "Journal Article\n"
+  end
+end
