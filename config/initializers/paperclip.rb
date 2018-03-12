64409f90674c546372fd64138f4ff8edea9fc00a
Sven Bohm <bohms@msu.edu>
2018-03-12 09:26:54 -0400

    remove uri adapter

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,12 @@
 Paperclip.options[:content_type_mappings] = {
   do: %w(text/plain)
 }
+
+worrisome_adapters = [Paperclip::UriAdapter, Paperclip::HttpUrlProxyAdapter, Paperclip::DataUriAdapter]
+
+Paperclip.io_adapters
+         .registered_handlers
+         .delete_if do |_block, handler_class|
+           worrisome_adapters.include?(handler_class)
+         end
+
