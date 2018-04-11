c51907a2f5bd3c353e657dee2275546e62880291
Sven Bohm <bohms@msu.edu>
2018-04-11 13:05:14 -0400

    move the prometheous collector to the new location

--- config.ru
+++ config.ru
@@ -1,4 +1,11 @@
 # This file is used by Rack-based servers to start the application.
 
+require 'rack'
+require 'prometheus/middleware/collector'
+require 'prometheus/middleware/exporter'
+
+use Prometheus::Middleware::Collector
+use Prometheus::Middleware::Exporter
+
 require ::File.expand_path('../config/environment',  __FILE__)
 run Metadata::Application
--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,8 @@
-require File.expand_path('../boot', __FILE__)
+require File.expand_path('/boot', __dir__)
 
 require 'rails/all'
 
 require 'rack'
-require 'prometheus/client/rack/collector'
-require 'prometheus/client/rack/exporter'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -25,11 +23,7 @@ module Metadata
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
-    config.cache_store = :file_store, "tmp/cache"
-
-    config.middleware.use Prometheus::Client::Rack::Collector
-    config.middleware.use Prometheus::Client::Rack::Exporter
+    config.cache_store = :file_store, 'tmp/cache'
   end
 end
 
