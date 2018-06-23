bc0335201de88401e81bcd6ea805282b9dfb666a
Sven Bohm <bohms@msu.edu>
2018-06-23 19:01:56 -0400

    try it with real_time indexes

--- app/indices/citation_index.rb
+++ app/indices/citation_index.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-ThinkingSphinx::Index.define :citation, with: :active_record do
-  indexes title
-  # indexes authors.sur_name, as: :authors, sortable: true, tye: :string
-  indexes publication
-  indexes pub_year, sortable: true
-  has website_id
+ThinkingSphinx::Index.define :citation, with: :real_time do
+  indexes title, sortable: true, type: :string
+  indexes authors.sur_name, as: :authors, sortable: true, tye: :string
+  indexes publication, type: :string
+  indexes pub_year, sortable: true, type: :datetime
+  has website_id, type: :integer
 end
