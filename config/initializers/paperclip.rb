965d04ba208716df5c35463a049b4082ab60be94
Sven Bohm <bohms@msu.edu>
2018-09-28 16:19:51 -0400

    added mime type for r code

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,12 +1,12 @@
 Paperclip.options[:content_type_mappings] = {
-  do: %w(text/plain)
+  do: %w[text/plain, text/x-matlab]
 }
 
-worrisome_adapters = [Paperclip::UriAdapter, Paperclip::HttpUrlProxyAdapter, Paperclip::DataUriAdapter]
+worrisome_adapters = [Paperclip::UriAdapter, Paperclip::HttpUrlProxyAdapter,
+                      Paperclip::DataUriAdapter]
 
 Paperclip.io_adapters
          .registered_handlers
          .delete_if do |_block, handler_class|
            worrisome_adapters.include?(handler_class)
          end
-
