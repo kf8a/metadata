6e2a4e549357e8dc1d628d460a1e9776f7311030
Sven Bohm <bohms@msu.edu>
2018-12-05 11:47:07 -0500

    style fixes

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -1,26 +1,24 @@
+# frozen_string_literal: true
+
 # Display units. This is not used much
 class UnitsController < ApplicationController
-  before_action :require_login, except: [:index, :show]
-  before_action :admin?, except: [:index, :show]
-  before_action :unit, only: [:edit, :update, :show]
-  before_action :custom_unit?, only: [:edit, :update]
+  before_action :require_login, except: %i[index show]
+  before_action :admin?, except: %i[index show]
+  before_action :unit, only: %i[edit update show]
+  before_action :custom_unit?, only: %i[edit update]
 
   def index
     @units = Unit.not_in_eml
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
-    if @unit.update_attributes(params[:unit])
-      flash[:notice] = 'Unit was succesfully updated'
-    end
+    flash[:notice] = 'Unit was succesfully updated' if @unit.update(params[:unit])
     respond_with @unit
   end
 
-  def show
-  end
+  def show; end
 
   private
 
