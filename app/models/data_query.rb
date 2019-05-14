7c4512e2abc2af909424a13cecf282b194cfe17a
Sven Bohm <bohms@msu.edu>
2019-05-14 10:23:50 -0400

    start batching at 20_000 rows and try to remove ordering from bigger tables

--- app/models/data_query.rb
+++ app/models/data_query.rb
@@ -4,7 +4,7 @@
 class DataQuery
   def self.find_in_batches_as_csv(query, options = {})
     options.assert_valid_keys(:start, :batch_size)
-    batch_size = options[:batch_size] || 5_000_000
+    batch_size = options[:batch_size] || 20_0000
 
     (0..count(query)).step(batch_size) do |offset|
       batch_query = "Select * from (#{query}) as foo limit #{batch_size} offset #{offset}"
@@ -17,7 +17,9 @@ class DataQuery
   end
 
   def self.to_csv_rows(results)
+    p results
     results.collect do |row|
+      p row
       CSV::Row.new(row.keys, row.values).to_s
     end.join
   end
