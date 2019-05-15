b6ebaa3650d25c56557d1044950bd9a6111a0800
Sven Bohm <bohms@msu.edu>
2019-05-15 10:14:54 -0400

    upgrade to factorybot

--- spec/models/data_query_spec.rb
+++ spec/models/data_query_spec.rb
@@ -2,19 +2,17 @@ require 'rails_helper'
 
 describe DataQuery do
   before(:each) do
-    50.times { FactoryGirl.create :datatable }
+    50.times { FactoryBot.create :datatable }
   end
 
   describe 'find in batches' do
-
     it 'yields several times' do
-      query = "select * from datatables"
+      query = 'select * from datatables'
       i = 0
-      data = DataQuery.find_in_batches_as_csv(query, {batch_size: 8}) do |a|
+      DataQuery.find_in_batches_as_csv(query, {batch_size: 8}) do |a|
         i += 1
       end
       assert i == 7
     end
   end
-
 end
