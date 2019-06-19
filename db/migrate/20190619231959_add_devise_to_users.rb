6a194e0aaa6a2b211f67e99838ec8ad43ef2e7af
Sven Bohm <bohms@msu.edu>
2019-06-19 19:48:37 -0400

    take out more existing fields that are used in devise

--- db/migrate/20190619231959_add_devise_to_users.rb
+++ db/migrate/20190619231959_add_devise_to_users.rb
@@ -8,11 +8,11 @@ class AddDeviseToUsers < ActiveRecord::Migration[5.2]
       # t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
+      # t.string   :reset_password_token
+      # t.datetime :reset_password_sent_at
 
       ## Rememberable
-      t.datetime :remember_created_at
+      # t.datetime :remember_created_at
 
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
@@ -38,7 +38,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[5.2]
     end
 
     # add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
+    # add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
