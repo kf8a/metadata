2eb835b9f0d5f982c683db643dddd82fb13c4e63
Sven Bohm <bohms@msu.edu>
2019-07-08 20:15:35 -0400

    fixed sign_in and sign_out links

--- db/migrate/20190619231959_add_devise_to_users.rb
+++ db/migrate/20190619231959_add_devise_to_users.rb
@@ -8,18 +8,18 @@ class AddDeviseToUsers < ActiveRecord::Migration[5.2]
       # t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
-      # t.string   :reset_password_token
-      # t.datetime :reset_password_sent_at
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
 
       ## Rememberable
-      # t.datetime :remember_created_at
+      t.datetime :remember_created_at
 
       ## Trackable
-      # t.integer  :sign_in_count, default: 0, null: false
-      # t.datetime :current_sign_in_at
-      # t.datetime :last_sign_in_at
-      # t.inet     :current_sign_in_ip
-      # t.inet     :last_sign_in_ip
+      t.integer  :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.inet     :current_sign_in_ip
+      t.inet     :last_sign_in_ip
 
       ## Confirmable
       # t.string   :confirmation_token
