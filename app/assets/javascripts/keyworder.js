3177e54f1a6d6a0f9e30cbd55bbf20946f1b1b25
sven <bohms@ideomatic.io>
2020-02-24 12:06:32 -0500

    decaffinate keyworder and use placeholder to set the hint

---
+++ app/assets/javascripts/keyworder.js
@@ -0,0 +1,14 @@
+$(function() {
+  const default_text = 'Search for core areas, keywords or people';
+
+  $('#keyworder').focus(function() {
+    if (this.value === default_text) {
+      return this.value = '';
+    }
+  });
+
+  return $('#keyworder').autocomplete({
+    source: '/datatables/suggest',
+    minLength: 2
+  });
+});
