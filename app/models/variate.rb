7b92f28c54c0e306c6a5dc4860c6f0250bf6252e
sven <bohms@ideomatic.io>
2020-08-27 12:03:46 -0400

    remove eml import code

--- app/models/variate.rb
+++ app/models/variate.rb
@@ -11,36 +11,6 @@ class Variate < ApplicationRecord
 
   scope :valid_for_eml, -> { where("measurement_scale != '' AND description != ''") }
 
-  def self.from_eml(variate_eml)
-    variate = Variate.new
-    variate.attributes_from_eml(variate_eml)
-    variate.unit_from_eml(variate_eml)
-    variate.save
-
-    variate
-  end
-
-  def attributes_from_eml(variate_eml)
-    self.name              = variate_eml.css('attributeName').text
-    self.description       = variate_eml.at_css('attributeDefinition, definition').text
-    self.measurement_scale = variate_eml.at_css('measurementScale')
-                                        .at_css('nominal, ordinal, interval, ratio, dateTime')
-                                        .name
-    self.precision         = variate_eml.at_css('precision').try(:text).try(:to_f)
-    self.data_type         = variate_eml.at_css('numberType').try(:text)
-    self.date_format       = variate_eml.at_css('formatString').try(:text)
-  end
-
-  def unit_from_eml(variate_eml)
-    if variate_eml.at_css('standardUnit')
-      unit_name = variate_eml.at_css('standardUnit').text
-      self.unit = Unit.find_or_create_by_name_and_in_eml(unit_name, true)
-    elsif variate_eml.at_css('customUnit')
-      unit_name = variate_eml.at_css('customUnit').text
-      self.unit = Unit.find_or_create_by_name_and_in_eml(unit_name, false)
-    end
-  end
-
   def to_eml(xml = ::Builder::XmlMarkup.new)
     @eml = xml
     @eml.attribute do
@@ -91,7 +61,7 @@ class Variate < ApplicationRecord
   end
 
   def eml_date_time
-    @eml.formatString date_format.blank? ? 'YYYY-MM-DD' : date_format
+    @eml.formatString date_format.presence || 'YYYY-MM-DD'
     @eml.dateTimePrecision '1'
     @eml.dateTimeDomain do
       @eml.bounds do
@@ -125,23 +95,3 @@ class Variate < ApplicationRecord
     end
   end
 end
-
-# == Schema Information
-#
-# Table name: variates
-#
-#  id                      :integer         not null, primary key
-#  name                    :string(255)
-#  datatable_id            :integer
-#  weight                  :integer
-#  description             :text
-#  missing_value_indicator :string(255)
-#  unit_id                 :integer
-#  measurement_scale       :string(255)
-#  data_type               :string(255)
-#  min_valid               :float
-#  max_valid               :float
-#  date_format             :string(255)
-#  precision               :float
-#  query                   :text
-#  variate_theme_id        :integer
