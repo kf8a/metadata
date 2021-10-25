12996f7af2ef1192024fa2a5bbf2456d62ccc283
sven <bohms@ideomatic.io>
2021-10-25 13:42:45 -0400

    update session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,12 +1,12 @@
+# frozen_string_literal: true
+
 # Be sure to restart your server when you modify this file.
 
 if Rails.env.development?
-  Metadata::Application.config.session_store :cookie_store, :key => '_metadata_session'
+  Metadata::Application.config.session_store :cookie_store, key: '_metadata_session'
 else
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
-  Metadata::Application.config.session_store :active_record_store, {
-#    :secure => true
-  }
+  # Use the database for sessions instead of the cookie-based default,
+  # which shouldn't be used to store highly confidential information
+  # (create the session table with "rake db:sessions:create")
+  Metadata::Application.config.session_store :active_record_store, key: '_metadata_session'
 end
