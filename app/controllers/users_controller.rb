e71f2d32ac10e0016e89c51598bbd654e999e908
Sven Bohm <bohms@hey.com>
2024-06-07 10:57:50 -0400

    restore CSRF protection

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@
 # handler interaction with the user class
 class UsersController < ApplicationController
   before_action :authenticate_user!, except: %i[show]
-  protect_from_forgery except: :show
+  # protect_from_forgery except: :show
 
   def index; end
 
