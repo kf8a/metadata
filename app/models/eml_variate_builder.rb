bee5effaf9ecf5889dfc6a3a370f1cb54f0b21d2
Sven Bohm <bohms@hey.com>
2025-03-27 09:16:33 -0400

    extract the eml variate building to it's own file.

---
+++ app/models/eml_variate_builder.rb
@@ -0,0 +1,104 @@
+# frozen_string_literal: true
+
+require 'rexml/document'
+
+# Builds EML representation of a Variate
+class EmlVariateBuilder
+  include REXML
+
+  def initialize(variate)
+    @variate = variate
+  end
+
+  def build(xml = ::Builder::XmlMarkup.new)
+    @eml = xml
+    @eml.attribute do
+      @eml.attributeName @variate.name
+      @eml.attributeDefinition @variate.description
+      eml_measurement_scale
+    end
+  end
+
+  private
+
+  def eml_measurement_scale
+    @eml.measurementScale do
+      scale = @variate.measurement_scale
+      scale = 'dateTime' if scale == 'datetime'
+
+      @eml.tag!(scale) do
+        measurement_scale_as_eml(scale)
+      end
+    end
+  end
+
+  def measurement_scale_as_eml(measurement_scale)
+    case measurement_scale
+    when 'interval' then eml_interval
+    when 'ratio' then eml_ratio
+    when 'ordinal' then nil
+    when 'nominal'then eml_nominal
+    when 'dateTime' then eml_date_time
+    end
+  end
+
+  def eml_unit
+    @eml.unit do
+      if @variate.unit.try(:in_eml)
+        @eml.standardUnit unit_name
+      else
+        @eml.customUnit unit_name
+      end
+    end
+  end
+
+  def unit_name
+    @variate.unit.try(:name)
+  end
+
+  def eml_date_time
+    @eml.formatString @variate.date_format.presence || 'YYYY-MM-DD'
+    @eml.dateTimePrecision '1'
+    @eml.dateTimeDomain do
+      @eml.bounds do
+        @eml.minimum '1987-4-18', 'exclusive' => 'true'
+      end
+    end
+  end
+
+  def eml_nominal
+    @eml.nonNumericDomain do
+      if @variate.ordinal_values.any?
+        @eml.enumeratedDomain do
+          @variate.ordinal_values.each do |ordinal_value|
+            @eml.codeDefinition do
+              @eml.code ordinal_value.code
+              @eml.definition ordinal_value.description
+            end
+          end
+        end
+      else
+        @eml.textDomain do
+          @eml.definition @variate.description
+        end
+      end
+    end
+  end
+
+  def eml_interval
+    eml_unit
+    eml_precision_and_number_type
+  end
+
+  def eml_ratio
+    eml_unit
+    eml_precision_and_number_type
+  end
+
+  def eml_precision_and_number_type
+    @eml.precision @variate.precision.to_s if @variate.precision
+    @eml.numericDomain do
+      @eml.numberType @variate.data_type
+    end
+  end
+end
--- app/models/variate.rb
+++ app/models/variate.rb
@@ -5,7 +5,6 @@ require 'rexml/document'
 # A variate is a variable that is measured or recorded. It represents the
 # column in a datatable.
 class Variate < ApplicationRecord
-  include REXML
   belongs_to :datatable, touch: true, optional: true
   belongs_to :unit, optional: true
   has_many :ordinal_values, dependent: :destroy
@@ -13,12 +12,7 @@ class Variate < ApplicationRecord
   scope :valid_for_eml, -> { where("measurement_scale != '' AND description != ''") }
 
   def to_eml(xml = ::Builder::XmlMarkup.new)
-    @eml = xml
-    @eml.attribute do
-      @eml.attributeName name
-      @eml.attributeDefinition description
-      eml_measurement_scale
-    end
+    EmlVariateBuilder.new(self).build(xml)
   end
 
   def human_name
@@ -27,84 +21,8 @@ class Variate < ApplicationRecord
 
   private
 
-  def eml_measurement_scale
-    @eml.measurementScale do
-      self.measurement_scale = 'dateTime' if measurement_scale == 'datetime'
-
-      @eml.tag!(measurement_scale) do
-        measurement_scale_as_eml(measurement_scale)
-      end
-    end
-  end
-
-  def measurement_scale_as_eml(measurement_scale)
-    case measurement_scale
-    when 'interval' then eml_interval
-    when 'ratio' then eml_ratio
-    when 'ordinal' then nil
-    when 'nominal'then eml_nominal
-    when 'nonNumeric' then eml_non_numeric
-    when 'dateTime' then eml_date_time
-    end
-  end
-
-  def eml_unit
-    @eml.unit do
-      if unit.try(:in_eml)
-        @eml.standardUnit unit_name
-      else
-        @eml.customUnit unit_name
-      end
-    end
-  end
-
   def unit_name
     unit.try(:name)
   end
 
-  def eml_date_time
-    @eml.formatString date_format.presence || 'YYYY-MM-DD'
-    @eml.dateTimePrecision '1'
-    @eml.dateTimeDomain do
-      @eml.bounds do
-        @eml.minimum '1987-4-18', 'exclusive' => 'true'
-      end
-    end
-  end
-
-  def eml_nominal
-    @eml.nonNumericDomain do
-      if ordinal_values.any?
-        @eml.enumeratedDomain do
-            ordinal_values.each do |ordinal_value|
-          @eml.codeDefinition do
-              @eml.code ordinal_value.code
-              @eml.definition ordinal_value.description
-            end
-          end
-        end
-      else
-        @eml.textDomain do
-          @eml.definition description
-      end
-    end
-    end
-  end
-
-  def eml_interval
-    eml_unit
-    eml_precision_and_number_type
-  end
-
-  def eml_ratio
-    eml_unit
-    eml_precision_and_number_type
-  end
-
-  def eml_precision_and_number_type
-    @eml.precision precision.to_s if precision
-    @eml.numericDomain do
-      @eml.numberType data_type
-    end
-  end
 end
